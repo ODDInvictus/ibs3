830e875665936be06fe4f68afe19e25ea40ba21f
Niels <git@nierot.com>
2023-01-28 15:05:55 +0100

    Error handling + ssr

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<h1>Oei!</h1>
+<p>
+  Daar is toch echt wel iets flink mis gegaan. Ik zou je een strafbak hiervoor geven maar helaas kan dat nog niet automatisch. Geluksvogel.
+</p>
+<p>
+  Hier is de foutmelding:
+  <code>{$page.error.message}</code>
+</p>
