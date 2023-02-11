6a61db68172d9601a377cb5207cf81a7edb77785
Niels <56843328+Nierot@users.noreply.github.com>
2023-02-11 19:04:46 +0100

    misc: verwijder voorbeeld

--- src/routes/(app)/instellingen/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { discordLogger } from '$lib/server/jobs/discord'
-import type { PageServerLoad } from './$types'
-
-export const load = (async () => {
-
-  await discordLogger.enqueue('Huts', { delay: 1000, })
-
-}) satisfies PageServerLoad
