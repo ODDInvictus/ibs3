d779ed1fae121ff84784def03a5481d23ed81ad2
Niels <56843328+Nierot@users.noreply.github.com>
2023-02-12 12:23:29 +0100

    feature: voeg /over toe

--- src/routes/(app)/over/+page.svelte
+++ src/routes/(app)/over/+page.svelte
@@ -1,5 +1,63 @@
-<h1>Over IBS</h1>
-IBS staat voor 'Irritant Brakke Software'. Met dit systeem kun je feuten pesten en ook jezelf!
+<script>
+  const date = new Date()
+</script>
 
-<h2>versie nr. 3.0.0</h2>
-<h2>git hash 4499f66fdab490d724b108d67c99378dfd55e8835124989576b9df0e5078e50a</h2>
+<h1>Welkom bij Invictus Bier Systeem, dé website voor O.D.D. Invictus!</h1>
+
+<p>
+  Invictus Bier Systeem is ontwikkeld om de activiteiten, 
+  strafbakken en financiële zaken van O.D.D. Invictus bij te houden. 
+  Met deze website kun je eenvoudig informatie bijhouden en delen met andere leden van het dispuut.
+</p>
+
+<p>
+  Onze website is gebouwd met TypeScript en SvelteKit, wat zorgt voor een snelle en efficiënte ervaring voor gebruikers. 
+  De gebruiksvriendelijke interface maakt het gemakkelijk om activiteiten te plannen, 
+  strafbakken bij te houden en financiële informatie te beheren.
+</p>
+
+<p>
+  Met Invictus Bier Systeem kun je je concentreren op wat echt belangrijk is: 
+  het vieren van leuke activiteiten en het versterken van de banden binnen het dispuut.
+  Laat de administratieve taken achterwege en geniet van meer tijd samen met je dispuutgenoten. 
+</p>
+
+<p>
+  Bedankt voor het kiezen van Invictus Bier Systeem! 
+  Wij zijn er trots op deel uit te maken van de reis van O.D.D. Invictus.
+</p>
+
+<footer>
+  <div>
+    <p>
+      Versie 3.0.0 <a href="https://github.com/ODDInvictus/ibs3/tree/54cc3c2efcc3dd27881cff9fa4634caaec14b205">(54cc3c2)</a>
+    </p>
+  </div>
+</footer>
+
+<style lang="scss">
+  h1 {
+    font-size: 2rem;
+  }
+
+  @media (min-width: 640px) {
+    h1 {
+      font-size: 2.5rem;
+    }
+
+    p {
+      max-width: 50vw;
+      margin-top: 1rem;
+    }
+  }
+
+  footer {
+    color: grey;
+    font-size: 0.8rem;
+
+    a:hover {
+      color: blue;
+    }
+  }
+
+</style>
