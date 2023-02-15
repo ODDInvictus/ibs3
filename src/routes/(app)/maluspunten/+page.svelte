15816b194b97031fc62ce49216b747bfdbdf5344
Naut de Vroome <82646766+Astronautilus14@users.noreply.github.com>
2023-02-15 16:28:15 +0100

    Strafbakkenmvp (#24)
    * Vechten met ts
    
    * UI eerste pagina
    
    * bezig met modals enzo
    
    * Stores zijn stom
    
    * Strafbakken mvp, je kan bakjes uitdelen en bakjes trekken met reden en datum.
    
    * Giver issue gehutst
    
    * Eerste merge
    
    * Alles stuk, doe fix zie todotje
    
    * Strafbak giver gehutst en ook even errorhandeling gedaan als je een bak probeert uit te delen maar er gaat iets finaal naar de grafgriep
    
    * bug fixes
    
    ---------
    
    Co-authored-by: Naut de Vroome <nautdevroome@gmail.com>

--- src/routes/(app)/maluspunten/+page.svelte
+++ src/routes/(app)/maluspunten/+page.svelte
@@ -1,79 +1,84 @@
 <script lang="ts">
-  import { page } from '$app/stores'
-  import { enhance, applyAction } from '$app/forms'
-	import StyledButton from '$lib/components/StyledButton.svelte';
+  import { page } from "$app/stores";
+  import { enhance, applyAction } from "$app/forms";
+  import StyledButton from "$lib/components/StyledButton.svelte";
 
-  let error = ''
+  let error = "";
 
   function formatDate(dateString: string) {
-    const date = new Date(dateString)
+    const date = new Date(dateString);
 
-    const hour = ('0' + date.getHours()).slice(-2)
-    const minute = ('0' + date.getMinutes()).slice(-2)
+    const hour = ("0" + date.getHours()).slice(-2);
+    const minute = ("0" + date.getMinutes()).slice(-2);
 
-    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${hour}:${minute}`
+    return `${date.getDate()}/${
+      date.getMonth() + 1
+    }/${date.getFullYear()} ${hour}:${minute}`;
   }
 </script>
 
-<div>
+<main>
   <h1>Maluspunten</h1>
 
   <hr />
 
-  <div id="form-container">
-    <form method="POST" use:enhance={event => {
-      return async ({ result, update }) => {
-        console.log(result)
-        if (result.type === 'failure') {
-          error = result.data.message
-        } else {
-          error = ''
-          console.log(result)
-          update()
-        }
-      }
-    }}>
+  <form-container>
+    <form
+      method="POST"
+      use:enhance={(event) => {
+        return async ({ result, update }) => {
+          console.log(result);
+          if (result.type === "failure") {
+            error = result.data?.message;
+          } else {
+            error = "";
+            console.log(result);
+            update();
+          }
+        };
+      }}
+    >
       <div>
         <label for="receiverId">Welke feut</label>
         <select name="receiverId">
           {#each $page.data.feuten as user}
-            <option value="{user.id}">{user.firstName}</option>
+            <option value={user.id}>{user.firstName}</option>
           {/each}
         </select>
       </div>
-  
-      <div>  
+
+      <div>
         <label for="reason">Waarom</label>
         <input type="text" name="reason" placeholder="Waarom" />
       </div>
-  
-      <div>  
+
+      <div>
         <label for="amount">Hoeveel maluspunten</label>
         <input type="number" name="amount" placeholder="Hoeveel" />
       </div>
-      
+
       <div>
         <label for="giverId">Gegeven door</label>
         <select name="giverId">
           {#each $page.data.members as user}
-            <option value="{user.id}">{user.firstName}</option>
+            <option value={user.id}>{user.firstName}</option>
           {/each}
         </select>
       </div>
-      
-      <div>  
+
+      <div>
         <StyledButton type="submit">Toevoegen</StyledButton>
       </div>
-      
+
       <div>
         {error}
       </div>
     </form>
-  </div>
+  </form-container>
 
   <hr />
 
-  <div id="table-container">
+  <table-container>
     <table>
       <thead>
         <tr>
@@ -87,22 +92,21 @@
       </thead>
       <tbody>
         {#each $page.data.maluspunten as punt, idx}
-        <tr>
-          <td>{punt.id}</td>
-          <td>{punt.receiver.firstName}</td>
-          <td>{punt.reason}</td>
-          <td>{punt.amount}</td>
-          <td>{punt.giver.nickname ?? punt.giver.firstName}</td>
-          <td>{formatDate(punt.dateCreated)}</td>
-        </tr>
+          <tr>
+            <td>{punt.id}</td>
+            <td>{punt.receiver.firstName}</td>
+            <td>{punt.reason}</td>
+            <td>{punt.amount}</td>
+            <td>{punt.giver.nickname ?? punt.giver.firstName}</td>
+            <td>{formatDate(punt.dateCreated)}</td>
+          </tr>
         {/each}
       </tbody>
     </table>
-  </div>
-</div>
+  </table-container>
+</main>
 
-<style>
-
+<style lang="scss">
   h1 {
     text-align: center;
     font-weight: 600;
@@ -118,34 +122,40 @@
     grid-template-columns: 1fr 1fr;
     grid-template-rows: 1fr 1fr;
     grid-gap: 1rem;
+
+    label {
+      color: black;
+      font-weight: 600;
+    }
+
+    input,
+    select {
+      margin-top: 0.2rem;
+      width: 100%;
+      border-radius: 5px;
+      border: 1px solid #d1d5db;
+
+      &:focus {
+        outline: none;
+        border: 1px solid #000;
+      }
+    }
   }
 
-  label {
-    color: black;
-    font-weight: 600;
-  }
-
-  input, select {
-    margin-top: 0.2rem;
-    width: 100%;
-    border-radius: 5px;
-    border: 1px solid #d1d5db;
-  }
-
-  input:focus , select:focus {
-    outline: none;
-    border: 1px solid #000;
-  }
-
-  #table-container {
+  table-container {
     overflow-y: auto;
+
+    table {
+      width: 100%;
+
+      tbody tr:nth-child(odd) {
+        background-color: #d3c0ff;
+      }
+    }
   }
 
-  table {
-    width: 100%;
-  }
-
-  td, th {
+  td,
+  th {
     padding: 0.5rem;
     text-align: left;
   }
--- src/routes/(app)/strafbakken/[slug]/+page.ts
+++
@@ -1,7 +0,0 @@
-import type { PageLoad } from './$types';
-
-export const load = (({params}) => {
-  return {
-    username: params.slug,
-  }
-}) satisfies PageLoad
