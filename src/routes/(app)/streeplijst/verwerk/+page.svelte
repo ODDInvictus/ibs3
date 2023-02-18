10d08cbaf73b53cc26a8f17c2bf50ca3ab598945
Niels Rotmensen <git@nierot.com>
2023-02-18 17:01:35 +0100

    steeplijst: geen mobiel lol

---
+++ src/routes/(app)/streeplijst/verwerk/+page.svelte
@@ -0,0 +1,31 @@
+
+<div id="root">
+  <h1>Verwerk een streeplijst</h1>
+
+</div>
+
+<div id="screen-warning">
+  <h1>Verwerk een streeplijst</h1>
+  <p>Deze pagina is alleen beschikbaar op een desktop scherm.</p>
+  <p>Mocht je dit wel voor mobiel willen, is er zeker wel een plekje voor je vrij in de bakkentrekkers ;)</p>
+</div>
+
+<style>
+  #root {
+    display: block;
+  }
+
+  #screen-warning {
+    display: none;
+  }
+
+  @media (min-width: 640px) {
+    #root {
+      display: none;
+    }
+
+    #screen-warning {
+      display: block;
+    }
+  }
+</style>
