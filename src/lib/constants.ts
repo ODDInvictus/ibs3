fed3774de6285e1e9aced402576d2013d2b31235
Niels <56843328+Nierot@users.noreply.github.com>
2023-02-28 19:24:37 +0100

    financial: add ledger ids

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,14 @@
 export const LDAP_IDS = {
   FEUTEN: 'feuten',
   MEMBERS: 'leden',
+  USERS: 'ibs-users',
+  SENAAT: 'senaat',
+  ADMINS: 'ibs-admins',
+  FINANCIE: 'financie'
+}
+
+export const LEDGER_IDS = {
+  SALE_BEER: 4501,
+  SALE_DEEPFRYER: 4502,
+  SALE_OTHER: 4503,
 }
