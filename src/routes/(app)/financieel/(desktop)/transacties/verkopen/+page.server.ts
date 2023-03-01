00b84a7743f94f5da295dab151b0547ef3a40e14
Niels <56843328+Nierot@users.noreply.github.com>
2023-03-01 09:27:21 +0100

    financial: convert sales to transactions

--- src/routes/(app)/financieel/(desktop)/transacties/verkopen/+page.server.ts
+++ src/routes/(app)/financieel/(desktop)/transacties/verkopen/+page.server.ts
@@ -1,6 +1,9 @@
 import type { PageServerLoad } from "./$types";
 import db from "$lib/server/db";
-import type { Actions } from '@sveltejs/kit';
+import { redirect, type Actions } from '@sveltejs/kit';
+import { FinancialPersonType, ProductType } from '@prisma/client';
+import { LEDGER_IDS } from '$lib/constants';
+import { fail } from '@sveltejs/kit';
 
 export const load = (async () => {
   const sales = await db.sale.findMany({
@@ -18,23 +21,76 @@ export const load = (async () => {
 export const actions = {
   default: async (event) => {
     console.log('[Sales] Converting all sales to Transactions!')
-    const sales = await db.sale.findMany()
+    const sales = await db.sale.findMany({
+      include: {
+        product: true,
+        person: true
+      }
+    })
+    const invictus = await db.financialPerson.findFirst({
+      where: {
+        type: FinancialPersonType.INVICTUS
+      }
+    })
+
+    if (!invictus) {
+      throw new Error('FinancialPerson with type INVICTUS not found!')
+    }
 
     const transactions = []
 
     for (const sale of sales) {
-      // TODO
+
+      const ledgerId = getLedgerId(sale.product.productType)
+
       const transaction = {
-        date: sale.date,
-        description: sale.description,
-        amount: sale.amount,
-        personId: sale.personId,
-        productId: sale.productId,
-        type: 'sale',
-        saleId: sale.id,
+        ledgerId,
+        description: "Verkoop: " + sale.product.name,
+        price: sale.product.price * sale.amount,
+        settled: false,
+        fromId: sale.person.id,
+        toId: invictus.id
       }
 
       transactions.push(transaction)
     }
+
+    try {
+      await db.transaction.createMany({
+        data: transactions
+      })
+    } catch (err) {
+      console.error(err)
+      throw fail(500, {
+        message: 'Failed to create transactions',
+        error: err
+      })
+    }
+
+    // Now we delete all sales
+    try {
+      await db.sale.deleteMany()
+    } catch (err) {
+    
+      console.error(err)
+      throw fail(500, {
+        message: 'Failed to delete sales',
+        error: err
+      })
+    }
+
+    // And redirect back to /financieel/transacties
+    throw redirect(300, '/financieel/transacties')
   }
 } satisfies Actions
+
+const getLedgerId = (type: ProductType) => {
+  switch (type) {
+    case ProductType.ALCOHOL:
+      return LEDGER_IDS.SALE_BEER
+    case ProductType.FOOD:
+      return LEDGER_IDS.SALE_FOOD
+    case ProductType.OTHER:
+      return LEDGER_IDS.SALE_OTHER
+  }
+}
