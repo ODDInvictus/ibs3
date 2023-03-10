8ac35e96e40c2097d16140d7f7e37b63ab600760
Niels <56843328+Nierot@users.noreply.github.com>
2023-03-10 09:45:57 +0100

    financial: add temp page

--- src/routes/(app)/financieel/+page.svelte
+++ src/routes/(app)/financieel/+page.svelte
@@ -1,1 +1,23 @@
 <h1>Financieel</h1>
+
+<p>Voor nu is deze pagina nog niet klaar</p>
+
+<a href="/financieel/transacties/">Transacties</a>
+<a href="/financieel/streeplijst/verwerk">Verwerk streeplijst</a>
+<a href="/financieel/transacties/verkopen">Verkopen</a>
+
+<style>
+
+  h1 {
+    font-size: 1.5rem;
+  }
+  a {
+    margin: 1rem 0;
+    display: block;
+    width: fit-content;
+  }
+  
+  a:hover {
+    text-decoration: underline;
+  }
+</style>
