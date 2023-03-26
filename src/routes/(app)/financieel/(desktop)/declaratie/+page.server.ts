0c842ec62f398a2398e7e38bacee4b7faa30d5d8
Niels Rotmensen <git@nierot.com>
2023-03-26 15:42:31 +0200

    financial: Add declaration form

--- src/routes/(app)/financieel/(desktop)/declaratie/+page.server.ts
+++ src/routes/(app)/financieel/(desktop)/declaratie/+page.server.ts
@@ -1,18 +1,20 @@
 import db from '$lib/server/db'
+import cdn from '$lib/server/cdn'
 import { fail } from '@sveltejs/kit';
-import type { PageServerLoad, Actions } from './$types'
+import type { Actions } from './$types'
+import { RECEIPT_BUCKET } from '$env/static/private';
 
-export const load = (async () => {
-  const products = await db.product.findMany({
-    where: {
-      isActive: true
-    }
-  })
+// export const load = (async () => {
+//   const products = await db.product.findMany({
+//     where: {
+//       isActive: true
+//     }
+//   })
 
-  return {
-    products
-  }
-}) satisfies PageServerLoad
+//   return {
+//     products
+//   }
+// }) satisfies PageServerLoad
 
 
 export const actions = {
@@ -22,14 +24,72 @@ export const actions = {
 
       const receipt = Buffer.from(await (data.receipt as Blob).arrayBuffer())
 
-      const filePath = 'uploads/declaratie'
+      // save declaration
+      if (!data.product || data.product === '') throw new Error('Product is verplicht')
+      if (!data.methode || data.methode === '') throw new Error('Methode is verplicht')
+      if (!data.prijs || data.prijs < 0.01) throw new Error('Prijs is verplicht')
+      if (!data.statiegeld || data.statiegeld < 0) throw new Error('Statiegeld is verplicht of onder 0')
+      if (!data.receipt) throw new Error('Bonnetje is verplicht')
 
-      // Save receipt and save declaration
+      data.prijs = parseFloat(data.prijs)
+      data.statiegeld = parseFloat(data.statiegeld)
 
-    } catch (err) {
-      throw fail(400, err.message ?? 'Internal Error')
+      const user = event.locals.user
+
+      const personData = await db.financialPersonDataUser.findUnique({
+        where: {
+          personId: user.id
+        }
+      })
+
+      if (!personData) throw new Error('Geen persoonsgegevens gevonden')
+
+
+      // Create object in database
+      const declaration = await db.declaration.create({
+        data: {
+          price: data.prijs,
+          reason: data.product,
+          methodOfPayment: data.methode,
+          personId: personData.personId,
+          }
+        })
+
+      if (data.statiegeld > 0) {
+        await db.declaration.create({
+          data: {
+            price: data.statiegeld,
+            reason: 'Statiegeld voor declaratie: ' + declaration.id,
+            methodOfPayment: data.methode,
+            personId: personData.personId
+          }
+        })
+      }
+
+      const filename = `receipt-${declaration.id}-${data.receipt.name}`
+
+      // save the receipt      
+      await cdn.putObject(RECEIPT_BUCKET, filename, receipt, {
+        'Content-Type': data.receipt.type
+      })
+
+      await db.declaration.update({
+        where: {
+          id: declaration.id
+        },
+        data: {
+          receipt: filename
+        }
+      })
+
+      return {
+        success: true,
+        message: 'Declaratie is opgeslagen'
+      }
+
+    } catch (err: Error) {
+      console.log('Error: ' + err.message)
+      return fail(400, { succes: false, message: err.message ?? 'Internal Error' })
     }
-
-
   }
 } satisfies Actions
--- src/routes/(app)/financieel/(desktop)/declaratie/+page.svelte
+++ src/routes/(app)/financieel/(desktop)/declaratie/+page.svelte
@@ -1,53 +1,145 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+  import type { PageData, ActionData } from './$types';
 
-  let type = 'product'
+  export let form: ActionData;
+
   let productData = {
-    product: 0,
-    price: 0,
-    statiegeld: 0,
-    amount: 0,
-    receipt: ''
+    product: form?.product ?? '',
+    prijs: form?.prijs ?? 0,
+    statiegeld: form?.statiegeld ?? 0,
+    methode: form?.methode ?? 'Eigen rekening'
   }
+
+  let files: FileList | null = null
+
+  $: if (files) {
+    showImage()
+  }
+
+  function showImage() {
+    const receipt = document.getElementById('receipt')! as HTMLInputElement
+    const image   = document.getElementById('receipt-image')! as HTMLImageElement
+
+    if (receipt) {
+      const fileList = receipt.files
+
+      if (!fileList || fileList.length === 0) return
+
+      const file = fileList[0]
+
+      const reader = new FileReader()
+
+      reader.onload = function (e) {
+        // @ts-expect-error Is gewoon string niet piepen
+        image.src = e.target?.result ?? ''
+      }
+
+      reader.readAsDataURL(file)
+    }
+  }
+
 </script>
 
 <h1>Doe een declaratie</h1>
 <p>Heb je bier gekocht, of wil je gewoon geld van ons? Doe dan een declaratie!</p>
 
-<div>
-  <input type="radio" name="type" bind:group={type} value="product" id="product">
-  <label for="product">Etenswaren</label>
-
-  <input type="radio" name="type" bind:group={type} value="anders" id="anders">
-  <label for="anders">Anders</label>
-</div>
-
-{#if type == 'product'}
-  <form method="POST" enctype="multipart/form-data">
-    <label for="product">Product</label>
-    <input type="text" name="product" id="product" bind:value={productData.product}>
-
-    <label for="statiegeld">Prijs</label>
-    <input type="number" name="statiegeld" id="statiegeld" bind:value={productData.price}>
-
-    <label for="statiegeld">Statiegeld</label>
-    <input type="number" name="statiegeld" id="statiegeld" bind:value={productData.statiegeld}>
-
-    <label for="amount">Aantal</label>
-    <input type="number" name="amount" id="amount" bind:value={productData.amount}>
-
-    <label for="receipt">Bon</label>
-    <input type="file" name="receipt" id="receipt" bind:value={productData.receipt}>
-
-    <button type="submit">Verstuur</button>
-  </form>
-{:else if type == 'anders'}
-  Andersform
+{#if form?.success}
+  <p class="success">{form.message}</p>
 {/if}
 
+{#if !form?.success}
+  <p class="error">{form?.message}</p>
+{/if}
+
+<form method="POST" enctype="multipart/form-data">
+
+  <label for="product">Wat heb je gekocht</label>
+  <input type="text" name="product" id="product" bind:value={productData.product}>
+
+  <label for="methode">Betaalmethode</label>
+  <input type="text" name="methode" id="methode" bind:value={productData.methode}>
+
+  <label for="statiegeld">Prijs</label>
+  <span class="input-euro">
+    <input type="number" class="euro" min="0.00" step=".01" name="prijs" id="prijs" bind:value={productData.prijs}>
+  </span>
+
+  <label for="statiegeld">Statiegeld</label>
+  <span class="input-euro">
+    <input type="number" class="euro" min="0.00" step=".01" name="statiegeld" id="statiegeld" bind:value={productData.statiegeld}>
+  </span>
+
+  <label for="receipt">Bon</label>
+  <input type="file" name="receipt" id="receipt" bind:files>
+  
+  <button
+    type="submit"
+    class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center transition">
+    Verstuur
+  </button>
+
+</form>
+
+<img src="" id="receipt-image" alt="Hier komt je bonnetje te staan" />
+
 <style lang="scss">
+  .error {
+    color: red;
+  }
+
+  .success {
+    color: purple;
+  }
+
   h1 {
     font-size: 1.5rem;
+  }
+
+  form {
+    margin-top: 1rem;
+    display: grid;
+    grid-template-columns: 100px 1fr;
+    gap: 1rem;
+
+    label {
+      font-weight: 600;
+      padding-top: 0.4rem;
+    }
+
+    button {
+      margin-bottom: 1rem;
+    }
+
+    input:not([type="file"]) {
+      padding: 0.5rem;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      outline: none;
+    }
+
+    input[type="file"] {
+      outline: none;
+      cursor: pointer;
+    }
+
+    input:focus:not([type="file"]) {
+      border: 1px solid purple;
+    }
+
+    .euro {
+      padding-left: 22px !important;
+    }
+  }
+
+  .input-euro {
+    position: relative;
+  }
+
+  .input-euro:before {
+    position: absolute;
+    content:"€";
+    top: 9px;
+    left: 9px;
   }
 
 </style>
