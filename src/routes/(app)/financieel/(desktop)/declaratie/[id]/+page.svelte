f8f2663b982481bff942af7e69ee64aa57bfb205
Niels Rotmensen <git@nierot.com>
2023-03-26 15:42:48 +0200

    financial: Begin working on declaration edit page

---
+++ src/routes/(app)/financieel/(desktop)/declaratie/[id]/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { page } from '$app/stores';
+	import { PUBLIC_RECEIPT_URL } from '$env/static/public';
+
+  console.log($page.data.declaration);
+</script>
+
+<div id="root">
+  <h1>Declaratie {$page.data.declaration.id}</h1>
+  <p>Het bewerken is helaas nog niet mogelijk, boeie ruurd</p>
+
+  <img src={PUBLIC_RECEIPT_URL + $page.data.declaration.receipt} alt="Helaas is hier geen bonnetje voor geupload :(" />
+</div>
+
+<style>
+  h1 {
+    font-size: 1.5rem;
+  }
+</style>
