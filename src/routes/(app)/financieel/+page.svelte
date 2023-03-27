13a36a27f7059fe51262241e9a7338d612eeeebe
Niels Rotmensen <git@nierot.com>
2023-03-27 20:29:14 +0200

    financial: add balance to /financial

--- src/routes/(app)/financieel/+page.svelte
+++ src/routes/(app)/financieel/+page.svelte
@@ -1,11 +1,22 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+
+  function formatPrice(price: number) {
+    return price.toLocaleString('nl-NL', { style: 'currency', currency: 'EUR' })
+  }
+</script>
+
 <h1>Financieel</h1>
 
-<p>Voor nu is deze pagina nog niet klaar</p>
+<p>Huidig saldo: {formatPrice($page.data.person.balance)}</p>
+
+<p>Je hebt de volgende opties: </p>
 
 <a href="/financieel/transacties/">Transacties</a>
 <a href="/financieel/streeplijst/verwerk">Verwerk streeplijst</a>
 <a href="/financieel/transacties/verkopen">Verkopen</a>
 <a href="/financieel/declaratie">Doe een declaratie</a>
+<a href="/financieel/declaratie/overzicht">Declaratie overzicht</a>
 
 <style>
 
