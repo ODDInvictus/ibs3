700b26dbe4f8f474cf40c8bd10e53c812ec0b168
Niels <56843328+Nierot@users.noreply.github.com>
2023-04-02 21:20:23 +0200

    app: Fix `app.d.t.s`

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,11 @@
 /// <reference types="@auth/sveltekit" />
-import { type Committee, type User } from 'prisma/prisma-client'
+import type { Committee, User } from 'prisma/prisma-client'
 
-declare namespace App {
-  interface Locals {
-    user: User
-    committees: Committee[]
+declare global {
+  declare namespace App {
+    interface Locals {
+      user: User
+      committees: Committee[]
+    }
   }
 }
-
-export {};
