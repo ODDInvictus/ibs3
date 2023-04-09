698e981e27e854a55e9092afd7102f64ac199358
Niels <git@nierot.com>
2023-04-09 21:16:47 +0200

    activity: Add an activity not found page

---
+++ src/routes/(app)/activiteit/[id]/+error.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+</script>
+
+<h1>
+  Oei! Activiteit {$page.params.id} niet gevonden.
+</h1>
+
+<hr />
+
+<p>Misschien is dit verwijderd of ben je zelf gewoon dom.</p>
+
+<style>
+  h1, p {
+    text-align: center;
+  }
+
+  hr {
+    margin: var(--hr-margin);
+  }
+</style>
