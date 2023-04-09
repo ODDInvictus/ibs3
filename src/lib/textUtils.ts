40ff0060ef9862492a098a2e7257953076f12d3c
Niels <git@nierot.com>
2023-04-09 21:17:25 +0200

    utils: Add `getSlug(title: string)`

--- src/lib/textUtils.ts
+++ src/lib/textUtils.ts
@@ -1,3 +1,7 @@
 export function formatPrice(price: number): string {
   return price.toLocaleString('nl-NL', { style: 'currency', currency: 'EUR' })
 }
+
+export function getSlug(title: string): string {
+  return title.toLowerCase().replace(/ /g, '-')
+}
