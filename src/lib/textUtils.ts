72037462f87afa51a8755136515a707ea9890fc2
Niels <git@nierot.com>
2023-04-10 17:02:49 +0200

    utils: Rework slug function

--- src/lib/textUtils.ts
+++ src/lib/textUtils.ts
@@ -2,6 +2,10 @@ export function formatPrice(price: number): string {
   return price.toLocaleString('nl-NL', { style: 'currency', currency: 'EUR' })
 }
 
-export function getSlug(title: string): string {
-  return title.toLowerCase().replace(/ /g, '-')
+export function getSlug(id: number, title: string): string {
+  return '' + id + '-' + title.toLowerCase().replace(/ /g, '-')
+}
+
+export function prettySlug(slug: string): string {
+  return slug.replace(/-/g, ' ')
 }
