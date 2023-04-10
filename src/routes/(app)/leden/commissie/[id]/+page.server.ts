a22a227aa783bc5f29138d49603b6a1ae8d4100a
Niels <git@nierot.com>
2023-04-10 17:04:12 +0200

    committee: Simple committee detail page

---
+++ src/routes/(app)/leden/commissie/[id]/+page.server.ts
@@ -0,0 +1,22 @@
+import db from '$lib/server/db'
+
+export const load = (async ({ params }) => {
+  const committee = await db.committee.findFirstOrThrow({
+    where: {
+      ldapId: params.id
+    },
+    include: {
+      CommitteeMember: {
+        include: {
+          member: true
+        }
+      }
+    }
+  })
+
+  console.log({ committee })
+
+  return {
+    committee
+  }
+})
