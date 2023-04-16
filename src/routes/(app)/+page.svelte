0a244f174fd79228d4a3da90c3e1788e5a3614cc
Niels Rotmensen <git@nierot.com>
2023-04-16 09:23:05 +0200

    wip: Add greeting to homescreen

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,1 +1,25 @@
-<h1>Overzicht</h1>
+<script lang="ts">
+  import { page } from '$app/stores'
+  import { env } from '$env/dynamic/public';
+
+  function getGreeting() {
+    const hour = new Date().getHours();
+    if (hour < 6) {
+      return 'Goedenacht';
+    } else if (hour < 12) {
+      return 'Goedemorgen';
+    } else if (hour < 18) {
+      return 'Goedemiddag';
+    } else {
+      return 'Goedenavond';
+    }
+  }
+</script>
+
+<h1>{getGreeting()}, {$page.data.user.firstName}!</h1>
+
+<p>Welkom bij Invictus Bier Systeem</p>
+
+<style>
+
+</style>
