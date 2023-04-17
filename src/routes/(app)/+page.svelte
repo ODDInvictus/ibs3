13dc049695c353a1041b9ff5824f1c66bbf1ca13
Niels Rotmensen <git@nierot.com>
2023-04-17 21:51:23 +0200

    feat: Add new homepage with a member of the day and a quote by Naut

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -14,12 +14,89 @@
       return 'Goedenavond';
     }
   }
+
+  const words = [
+    'Weezer', 
+    'CaptainSparklez', 
+    'Minecraft',
+    'Maarten Marcusse',
+    'Diederik',
+    'Naut',
+    'Bier',
+    'Invictus',
+    'Coldplay',
+    'JoyRadio',
+    'Kerst',
+    'Abstracte Algebra',
+    'KB45',
+    'http://localhost:5173',
+    'marktplaats.nl',
+    'kaas.nl',
+    'de ultieme kaasbeleving',
+    'discord',
+    'docker',
+    'phpMyAdmin',
+    'een emmer van de trap tyfen',
+    'de frituurpan',
+    'zijn fiets',
+    'de vestingbar',
+    'kunnen fietsen',
+    'een koe',
+    'de SmartXP',
+    '130 rijden op de vluchtstrook',
+    'de mac',
+    'de mek',
+  ]
+
+  function getRandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
+  }
 </script>
 
 <h1>{getGreeting()}, {$page.data.user.firstName}!</h1>
 
 <p>Welkom bij Invictus Bier Systeem</p>
 
-<style>
+<hr />
 
+<div id="quote">
+  <blockquote>"Ik ben toch wel zat ... ik zag dit toch wel aan voor Weezer" - Naut <i>over {getRandomWord()}</i></blockquote>
+</div>
+
+<div id="lid">
+  <h1>Lid van de dag!</h1>
+
+  <img src={env.PUBLIC_UPLOAD_URL + '/users/' + $page.data.member?.picture ?? 'diederik_cropped.jpg'} alt="Diederik?">
+  <h2>{$page.data.member?.firstName}</h2>
+</div>
+
+
+
+<style>
+  hr {
+    margin: 2rem 0.5rem;
+  }
+
+  #quote {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 2rem;
+  }
+
+  #lid {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    justify-content: center;
+    align-items: center;
+    margin-top: 2rem;
+  }
+
+  #lid > img {
+    border: 2px solid var(--primary-color);
+    border-radius: 100%;
+    width: 100%;
+    max-width: 500px;
+  }
 </style>
