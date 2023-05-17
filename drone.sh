0249bfa0ec70bc7098f5f5696fb6127e3e912822
Niels <56843328+Nierot@users.noreply.github.com>
2023-05-17 12:06:18 +0200

    drone: run sh

--- drone.sh
+++ drone.sh
@@ -3,6 +3,6 @@
 export PUBLIC_GIT_REV=$(echo $DRONE_COMMIT)
 export PUBLIC_GIT_REV_SHORT=$(echo `expr $DRONE_COMMIT 0 7`)
 export PUBLIC_GIT_BRANCH=$(echo $DRONE_BRANCH)
-export PUBLIC_BUILD_NUMBER=
+export PUBLIC_BUILD_NUMBER=$(echo $DRONE_BUILD_NUMBER)
 export PUBLIC_BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 export PUBLIC_BUILD_LINK=$(echo $DRONE_BUILD_LINK)
