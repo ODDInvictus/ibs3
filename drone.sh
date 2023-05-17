246e28ab156f94d8c45529d96040bc532785eded
Niels <56843328+Nierot@users.noreply.github.com>
2023-05-17 12:10:29 +0200

    drone: fix `substr`

--- drone.sh
+++ drone.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export PUBLIC_GIT_REV=$(echo $DRONE_COMMIT)
-export PUBLIC_GIT_REV_SHORT=$(echo `expr $DRONE_COMMIT 0 7`)
+export PUBLIC_GIT_REV_SHORT=$(echo `expr substr $DRONE_COMMIT 1 7`)
 export PUBLIC_GIT_BRANCH=$(echo $DRONE_BRANCH)
 export PUBLIC_BUILD_NUMBER=$(echo $DRONE_BUILD_NUMBER)
 export PUBLIC_BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
