12fbed1e9fd20f9b4505de9ddf04c53cb4ec5270
Niels <git@nierot.com>
2023-07-15 20:24:29 +0200

    temp: Temporary admin page

---
+++ src/routes/(app)/admin/email/+page.svelte
@@ -0,0 +1,19 @@
+<h1>Email</h1>
+
+<hr />
+
+<a href="/admin/email/alias">Beheer aliassen</a>
+
+<style>
+	h1 {
+		text-align: center;
+	}
+
+	hr {
+		margin: 0.5rem 0;
+	}
+
+	a {
+		color: var(--primary-color);
+	}
+</style>
