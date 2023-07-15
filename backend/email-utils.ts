ad8ca0f38cedcb8c8cdb823cc38386aa6159fb78
Niels <git@nierot.com>
2023-07-15 13:26:22 +0200

    feat: Let the backend send emails

---
+++ backend/email-utils.ts
@@ -0,0 +1,31 @@
+import { createTransport } from 'nodemailer'
+
+const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD } = process.env
+
+if (!SMTP_HOST || !SMTP_PORT || !SMTP_USER || !SMTP_PASSWORD) {
+  console.error('Missing required environment variables for emailer')
+}
+
+const transporter = createTransport({
+  host: SMTP_HOST,
+  port: parseInt(SMTP_PORT || '465'),
+  secure: false,
+  tls: {
+    ciphers: 'SSLv3'
+  },
+  auth: {
+    user: SMTP_USER,
+    pass: SMTP_PASSWORD
+  }
+})
+
+export async function sendEmailNotification(title: string, text: string, receiver: string) {
+  const info = await transporter.sendMail({
+    from: "IBS Backend Notificaties <ibs.backend@oddinvictus.nl>",
+    to: receiver,
+    subject: title,
+    text: text
+  })
+
+  console.log(`Email sent to ${receiver}: ${info.messageId}`)
+}
