0fefcae558c33533d205cd7a30b2bbed67ae2119
Niels <git@nierot.com>
2023-07-15 13:27:37 +0200

    feat: Add email support to IBS

---
+++ prisma/migrations/20230715112517_email_support/migration.sql
@@ -0,0 +1,72 @@
+-- DropIndex
+DROP INDEX `Link_link_key` ON `Link`;
+
+-- AlterTable
+ALTER TABLE `Link` MODIFY `link` TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Settings` ALTER COLUMN `updatedAt` DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE `User` ADD COLUMN `isActive` BOOLEAN NOT NULL DEFAULT true,
+    ADD COLUMN `personalEmail` VARCHAR(191) NULL;
+
+-- CreateTable
+CREATE TABLE `EmailAlias` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `alias` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `EmailAlias_alias_key`(`alias`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `EmailAliasCommittee` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `emailAliasId` INTEGER NOT NULL,
+    `committeeId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `EmailAliasUser` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `emailAliasId` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `EmailContact` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `address` VARCHAR(191) NOT NULL,
+    `emailAliasId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `EmailContact_address_key`(`address`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `EmailAliasCommittee` ADD CONSTRAINT `EmailAliasCommittee_emailAliasId_fkey` FOREIGN KEY (`emailAliasId`) REFERENCES `EmailAlias`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `EmailAliasCommittee` ADD CONSTRAINT `EmailAliasCommittee_committeeId_fkey` FOREIGN KEY (`committeeId`) REFERENCES `Committee`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `EmailAliasUser` ADD CONSTRAINT `EmailAliasUser_emailAliasId_fkey` FOREIGN KEY (`emailAliasId`) REFERENCES `EmailAlias`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `EmailAliasUser` ADD CONSTRAINT `EmailAliasUser_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `EmailContact` ADD CONSTRAINT `EmailContact_emailAliasId_fkey` FOREIGN KEY (`emailAliasId`) REFERENCES `EmailAlias`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
