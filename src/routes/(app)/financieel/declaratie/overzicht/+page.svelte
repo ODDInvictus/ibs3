9df16ada81500df609cff45fed3bcf732e7e303a
Niels <git@nierot.com>
2023-07-16 21:01:39 +0200

    fmt: Run formatter

--- src/routes/(app)/financieel/declaratie/overzicht/+page.svelte
+++ src/routes/(app)/financieel/declaratie/overzicht/+page.svelte
@@ -1,165 +1,165 @@
 <script lang="ts">
-  import { page } from '$app/stores'
-  import { XCircle, PencilSquare, Check } from 'svelte-heros-v2'
+	import { page } from '$app/stores';
+	import { XCircle, PencilSquare, Check } from 'svelte-heros-v2';
 
-  let error = ''
+	let error = '';
 
-  function formatPrice(price: number) {
-    return price.toLocaleString('nl-NL', { style: 'currency', currency: 'EUR' })
-  }
+	function formatPrice(price: number) {
+		return price.toLocaleString('nl-NL', { style: 'currency', currency: 'EUR' });
+	}
 
-  async function process(id: number, type: 'accept' | 'decline') {
-    const accept = confirm('Weet je zeker dat je deze declaratie wilt ' + (type === 'accept' ? 'goedkeuren?' : 'afkeuren?'))
+	async function process(id: number, type: 'accept' | 'decline') {
+		const accept = confirm(
+			'Weet je zeker dat je deze declaratie wilt ' +
+				(type === 'accept' ? 'goedkeuren?' : 'afkeuren?')
+		);
 
-    if (!accept) return
-    await fetch('', {
-      method: 'POST',
-      body: JSON.stringify({ id, type }),
-    })
-    .then(res => res.json())
-    .then(res => {
-      console.log(res)
-      if (res.status !== 200) {
-        error = res.message
-      } else {
-        removeRow(id)
-      }
-    })
-    .catch(err => error = err.message)
-  }
+		if (!accept) return;
+		await fetch('', {
+			method: 'POST',
+			body: JSON.stringify({ id, type })
+		})
+			.then((res) => res.json())
+			.then((res) => {
+				if (res.status !== 200) {
+					error = res.message;
+				} else {
+					removeRow(id);
+				}
+			})
+			.catch((err) => (error = err.message));
+	}
 
-  function removeRow(id: number) {
-    // select the tr where x-id = id
-    const row = document.querySelector(`tr[data-id="${id}"]`)
-    row?.remove()
-  }
+	function removeRow(id: number) {
+		// select the tr where x-id = id
+		const row = document.querySelector(`tr[data-id="${id}"]`);
+		row?.remove();
+	}
 </script>
 
 <div id="root">
-  <h1>Declaratie overzicht</h1>
+	<h1>Declaratie overzicht</h1>
 
-  <hr />
+	<hr />
 
-  <p>
-    Hieronder staan alle declaraties die zijn gedaan door gebruikers van IBS.
-    Wanneer je een declaratie goedkeurd zal het saldo worden aangepast.
-  </p>
+	<p>
+		Hieronder staan alle declaraties die zijn gedaan door gebruikers van IBS. Wanneer je een
+		declaratie goedkeurd zal het saldo worden aangepast.
+	</p>
 
-  <p>
-    <a href="/financieel/declaratie">Wil je een declaratie doen?</a>
-  </p>
+	<p>
+		<a href="/financieel/declaratie">Wil je een declaratie doen?</a>
+	</p>
 
-  <p class="error">
-    {#if error}
-      {error}
-    {/if}
-  </p>
+	<p class="error">
+		{#if error}
+			{error}
+		{/if}
+	</p>
 
-  <table>
-    <thead>
-      <tr>
-        <th>Id</th>
-        <th>Prijs</th>
-        <th>Datum</th>
-        <th>Persoon</th>
-        <th>Reden</th>
-        <th>Betaalwijze</th>
-        <th>Acties</th>
-      </tr>
-    </thead>
-    <tbody>
-      {#if !$page.data.declarations.length}
-        <tr>
-          <td colspan="7">
-            <p id="no-decla">Geen declaraties gevonden</p>
-            <a href="/financieel/declaratie" class="link">Wil je een declaratie doen?</a>
-          </td>
-        </tr>
-      {/if}
-      {#each $page.data.declarations as declaration}
-        <tr data-id={declaration.id}>
-          <td>{declaration.id}</td>
-          <td class="price">{formatPrice(declaration.price)}</td>
-          <td>{new Date(declaration.createdAt).toLocaleString('nl-NL')}</td>
-          <td>{declaration.person.name}</td>
-          <td>{declaration.reason}</td>
-          <td>{declaration.methodOfPayment}</td>
-          <td class="actions">
-            <button on:click={() => process(declaration.id, 'accept')}>
-              <Check class="text-green-500" />
-            </button>
-            <button on:click={() => process(declaration.id, 'decline')}>
-              <XCircle class="text-red-500" />
-            </button>
-            <a href="/financieel/declaratie/{declaration.id}">
-              <PencilSquare class="text-purple-500"/>
-            </a>
-          </td>
-        </tr>
-      {/each}
-    </tbody>
-  </table>
+	<table>
+		<thead>
+			<tr>
+				<th>Id</th>
+				<th>Prijs</th>
+				<th>Datum</th>
+				<th>Persoon</th>
+				<th>Reden</th>
+				<th>Betaalwijze</th>
+				<th>Acties</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#if !$page.data.declarations.length}
+				<tr>
+					<td colspan="7">
+						<p id="no-decla">Geen declaraties gevonden</p>
+						<a href="/financieel/declaratie" class="link">Wil je een declaratie doen?</a>
+					</td>
+				</tr>
+			{/if}
+			{#each $page.data.declarations as declaration}
+				<tr data-id={declaration.id}>
+					<td>{declaration.id}</td>
+					<td class="price">{formatPrice(declaration.price)}</td>
+					<td>{new Date(declaration.createdAt).toLocaleString('nl-NL')}</td>
+					<td>{declaration.person.name}</td>
+					<td>{declaration.reason}</td>
+					<td>{declaration.methodOfPayment}</td>
+					<td class="actions">
+						<button on:click={() => process(declaration.id, 'accept')}>
+							<Check class="text-green-500" />
+						</button>
+						<button on:click={() => process(declaration.id, 'decline')}>
+							<XCircle class="text-red-500" />
+						</button>
+						<a href="/financieel/declaratie/{declaration.id}">
+							<PencilSquare class="text-purple-500" />
+						</a>
+					</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 </div>
 
 <style lang="scss">
-  .actions {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-  }
+	.actions {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
 
-  @media (max-width: 640px) {
-    #root {
-      overflow: scroll;
-    }
+	@media (max-width: 640px) {
+		#root {
+			overflow: scroll;
+		}
 
-    table {
-      width: 100%;
-      overflow: scroll;
-    }
+		table {
+			width: 100%;
+			overflow: scroll;
+		}
 
-    tr {
-      height: 0;
-    }
+		tr {
+			height: 0;
+		}
 
-    td {
-      height: 0;
-    }
+		td {
+			height: 0;
+		}
 
+		.actions {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
 
-    .actions {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
+			height: 100%;
+		}
+	}
 
-      height: 100%;
-    }
-  }
+	h1 {
+		text-align: center;
+	}
 
-  h1 {
-    text-align: center;
-  }
+	hr {
+		margin: var(--hr-margin);
+	}
 
-  hr {
-    margin: var(--hr-margin);
-  }
+	a {
+		color: var(--link-color);
+	}
 
-  a {
-    color: var(--link-color);
-  }
+	.error {
+		color: var(--error-color);
+	}
 
-  .error {
-    color: var(--error-color);
-  }
+	#no-decla {
+		font-size: 1.1rem;
+	}
 
-  #no-decla {
-    font-size: 1.1rem;
-  }
-
-  .link:hover {
-    text-decoration: underline;
-    cursor: pointer;
-  }
-
+	.link:hover {
+		text-decoration: underline;
+		cursor: pointer;
+	}
 </style>
