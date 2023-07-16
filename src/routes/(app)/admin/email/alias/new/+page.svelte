a03fd28f04227f74e398b770caf0648ea2154111
Niels <git@nierot.com>
2023-07-16 21:00:52 +0200

    dev: Start working on alias creation page

---
+++ src/routes/(app)/admin/email/alias/new/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { toast } from '$lib/notification';
+	import Edit from '~icons/tabler/edit';
+</script>
+
+<h1>Nieuwe alias</h1>
+
+<hr />
+
+<style lang="scss">
+	h1 {
+		text-align: center;
+	}
+
+	a {
+		color: var(--primary-color);
+	}
+
+	hr {
+		margin: 0.5rem 0;
+	}
+</style>
