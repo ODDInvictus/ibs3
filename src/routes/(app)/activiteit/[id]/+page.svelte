e2887c32465c65dff32b270a532906cd6d4bc416
Niels <git@nierot.com>
2023-07-18 15:06:00 +0200

    chore: Replace `alert()` with `toast()` or `confirm()`

--- src/routes/(app)/activiteit/[id]/+page.svelte
+++ src/routes/(app)/activiteit/[id]/+page.svelte
@@ -1,356 +1,395 @@
 <script lang="ts">
-  import { page } from '$app/stores'
-  import { env } from '$env/dynamic/public';
-  import MapPin from '~icons/tabler/map-pin'
-  import Clock from '~icons/tabler/clock'
-  import Calendar from '~icons/tabler/calendar'
-  import CalendarPlus from '~icons/tabler/calendar-plus'
-  import UsersGroup from '~icons/tabler/users-group'
-  import ExternalLink from '~icons/tabler/external-link'
-  import AccessibleOff from '~icons/tabler/accessible-off'
+	import { page } from '$app/stores';
+	import { env } from '$env/dynamic/public';
+	import MapPin from '~icons/tabler/map-pin';
+	import Clock from '~icons/tabler/clock';
+	import Calendar from '~icons/tabler/calendar';
+	import CalendarPlus from '~icons/tabler/calendar-plus';
+	import UsersGroup from '~icons/tabler/users-group';
+	import ExternalLink from '~icons/tabler/external-link';
+	import AccessibleOff from '~icons/tabler/accessible-off';
 	import { getSlug } from '$lib/textUtils';
 	import UserCard from './UserCard.svelte';
 	import { generateICal } from '$lib/utils';
+	import { toast } from '$lib/notification';
 
-  const activity  = $page.data.activity
-  let attending   = $page.data.attending
-  $: bij    = attending.filter(a => a.isAttending).map(a => a.user)
-  $: unsure = attending.filter(a => {
-      if (bij.includes(a.user)) return false
+	const activity = $page.data.activity;
+	let attending = $page.data.attending;
+	$: bij = attending.filter((a) => a.isAttending).map((a) => a.user);
+	$: unsure = attending
+		.filter((a) => {
+			if (bij.includes(a.user)) return false;
 
-      let cr = new Date(a.createdAt)
-      let ua = new Date(a.updatedAt)
+			let cr = new Date(a.createdAt);
+			let ua = new Date(a.updatedAt);
 
-      cr.setMilliseconds(0)
-      ua.setMilliseconds(0)
-      cr.setSeconds(0)
-      ua.setSeconds(0)
+			cr.setMilliseconds(0);
+			ua.setMilliseconds(0);
+			cr.setSeconds(0);
+			ua.setSeconds(0);
 
-      return cr.getTime() === ua.getTime()
-    }).map(a => a.user)
+			return cr.getTime() === ua.getTime();
+		})
+		.map((a) => a.user);
 
-  $: notBij = attending.map(a => a.user).filter(u => !bij.includes(u) && !unsure.includes(u))
+	$: notBij = attending.map((a) => a.user).filter((u) => !bij.includes(u) && !unsure.includes(u));
 
-  function formatTime(time: string) {
-    const date = new Date(time)
-    return date.toLocaleTimeString('nl-NL', { hour: '2-digit', minute: '2-digit' })
-  }
+	function formatTime(time: string) {
+		const date = new Date(time);
+		return date.toLocaleTimeString('nl-NL', { hour: '2-digit', minute: '2-digit' });
+	}
 
-  function formatDate(time: string, endTime: string) {
-    const date = new Date(time)
-    const end = new Date(endTime)
+	function formatDate(time: string, endTime: string) {
+		const date = new Date(time);
+		const end = new Date(endTime);
 
-    // If the activity is longer than 12 hours, show the end date
-    if (end.getTime() - date.getTime() > 12 * 60 * 60 * 1000) {
-      return date.toLocaleDateString('nl-NL', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' })
-            + ' - ' + 
-            end.toLocaleDateString('nl-NL', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' })
-    }
+		// If the activity is longer than 12 hours, show the end date
+		if (end.getTime() - date.getTime() > 12 * 60 * 60 * 1000) {
+			return (
+				date.toLocaleDateString('nl-NL', {
+					weekday: 'long',
+					day: 'numeric',
+					month: 'long',
+					year: 'numeric'
+				}) +
+				' - ' +
+				end.toLocaleDateString('nl-NL', {
+					weekday: 'long',
+					day: 'numeric',
+					month: 'long',
+					year: 'numeric'
+				})
+			);
+		}
 
-    return date.toLocaleDateString('nl-NL', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' })
-  }
+		return date.toLocaleDateString('nl-NL', {
+			weekday: 'long',
+			day: 'numeric',
+			month: 'long',
+			year: 'numeric'
+		});
+	}
 
-  async function setAttending(status: boolean) {
-    // First check if the user is attending
-    const a = attending.find(a => a.user.ldapId == $page.data.user.ldapId)
+	async function setAttending(status: boolean) {
+		// First check if the user is attending
+		const a = attending.find((a) => a.user.ldapId == $page.data.user.ldapId);
 
-    if (a.isAttending === status) {
-      // The user is already attending/not attending, so do nothing
-      return
-    }
+		if (a.isAttending === status) {
+			// The user is already attending/not attending, so do nothing
+			return;
+		}
 
-    // Send a request to the server to update the attending status
-    await fetch('', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        status,
-        activityId: activity.id
-      })
-    }).catch(err => {
-      console.error(err)
-      alert(err.message)
-    })
+		// Send a request to the server to update the attending status
+		await fetch('', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				status,
+				activityId: activity.id
+			})
+		})
+			.then(() => {
+				toast({
+					title: status ? 'Gezellig!' : 'Jammer!',
+					message: `Je  ${status ? 'aanwezigheid' : 'afwezigheid'} is opgeslagen`,
+					type: status ? 'success' : 'info'
+				});
+			})
+			.catch((err) => {
+				toast({
+					title: 'Oei!',
+					message: 'Er ging iets mis bij het opslaan van je aanwezigheid',
+					type: 'error'
+				});
+				console.error(err);
+			});
 
-    // Update the attending status
-    a.isAttending = status
-    attending = attending
-  }
+		// Update the attending status
+		a.isAttending = status;
+		attending = attending;
+	}
 
-  function generateIcal() {
-    const startTime = new Date(activity.startTime)
-    const endTime = new Date(activity.endTime)
-    const ical = generateICal({
-      title: activity.name,
-      eventId: activity.id,
-      description: activity.description,
-      location: activity.location?.name,
-      startTime,
-      endTime,
-      url: window.location.href
-    })
+	function generateIcal() {
+		const startTime = new Date(activity.startTime);
+		const endTime = new Date(activity.endTime);
+		const ical = generateICal({
+			title: activity.name,
+			eventId: activity.id,
+			description: activity.description,
+			location: activity.location?.name,
+			startTime,
+			endTime,
+			url: window.location.href
+		});
 
-    // now let's save the file
-    const downloadLink = document.createElement('a')
-    downloadLink.href = ical
-    downloadLink.download = `ibs-activiteit-${activity.id}.ical`
-    document.body.appendChild(downloadLink)
-    downloadLink.click()
-    document.body.removeChild(downloadLink)
-  }
-
+		// now let's save the file
+		const downloadLink = document.createElement('a');
+		downloadLink.href = ical;
+		downloadLink.download = `ibs-activiteit-${activity.id}.ical`;
+		document.body.appendChild(downloadLink);
+		downloadLink.click();
+		document.body.removeChild(downloadLink);
+	}
 </script>
 
 <div>
-  <div id="title">
-    <h1>{activity.name}</h1>
-    <hr />
-  </div>
+	<div id="title">
+		<h1>{activity.name}</h1>
+		<hr />
+	</div>
 
+	<div class="cols">
+		<div id="left" class="col">
+			{#if activity.image == null}
+				<img
+					src={env.PUBLIC_UPLOAD_URL + 'activities/activiteit-0-logo.png'}
+					alt="Placeholder mist?"
+				/>
+			{:else}
+				<img
+					src={env.PUBLIC_UPLOAD_URL + 'activities/' + activity.image}
+					alt="Geen plaatje geupload :("
+				/>
+			{/if}
 
-  <div class="cols">
-    <div id="left" class="col">
-      {#if activity.image == null}
-        <img src={env.PUBLIC_UPLOAD_URL + 'activities/activiteit-0-logo.png'} alt="Placeholder mist?"/>
-      {:else}
-        <img src={env.PUBLIC_UPLOAD_URL + 'activities/' + activity.image} alt="Geen plaatje geupload :(" />
-      {/if}
+			<h2>{activity.name}</h2>
 
-      <h2>{activity.name}</h2>
+			<hr />
 
-      <hr />
+			<div class="row">
+				<UsersGroup />
+				<p>
+					Georganiseerd door:
+					<a href="/leden/commissie/{activity.organisedBy.ldapId}">{activity.organisedBy.name}</a>
+				</p>
+			</div>
 
-      <div class="row">
-        <UsersGroup />
-        <p>
-          Georganiseerd door: 
-          <a href="/leden/commissie/{activity.organisedBy.ldapId}">{activity.organisedBy.name}</a>
-        </p>
-      </div>
+			<hr />
 
-      <hr />
+			<div class="row">
+				<MapPin />
+				<p>
+					{#if activity.location !== null}
+						<a href="/locatie/{activity.location.id}">{activity.location.name}</a>
+					{:else}
+						Nog geen locatie bekend
+					{/if}
+				</p>
+			</div>
 
-      <div class="row">
-        <MapPin /> 
-        <p>
-          {#if activity.location !== null}
-            <a href="/locatie/{activity.location.id}">{activity.location.name}</a>
-          {:else}
-            Nog geen locatie bekend
-          {/if}
-        </p>
-      </div>
+			<hr />
 
-      <hr />
+			<div class="row">
+				<Calendar />
+				<p>{formatDate(activity.startTime, activity.endTime)}</p>
+			</div>
 
-      <div class="row">
-        <Calendar />
-        <p>{formatDate(activity.startTime, activity.endTime)}</p>
-      </div>
+			<hr />
 
-      <hr />
+			<div class="row">
+				<Clock />
+				<p>{formatTime(activity.startTime)} - {formatTime(activity.endTime)}</p>
+			</div>
 
-      <div class="row">
-        <Clock />
-        <p>{formatTime(activity.startTime)} - {formatTime(activity.endTime)}</p>
-      </div>
-      
-      {#if activity.url}
-      <hr />
+			{#if activity.url}
+				<hr />
 
-      <div class="row">
-        <ExternalLink />
-        <p>
-          <a href="{activity.url}">Meer informatie</a>
-        </p>
-      </div>
-      {/if}
+				<div class="row">
+					<ExternalLink />
+					<p>
+						<a href={activity.url}>Meer informatie</a>
+					</p>
+				</div>
+			{/if}
 
-      {#if activity.membersOnly}
-      <hr />
+			{#if activity.membersOnly}
+				<hr />
 
-      <div class="row">
-        <AccessibleOff />
-        <p>Alleen voor leden</p>
-      </div>
-      {/if}
+				<div class="row">
+					<AccessibleOff />
+					<p>Alleen voor leden</p>
+				</div>
+			{/if}
 
-      <hr />
+			<hr />
 
-      <div class="row">
-        <CalendarPlus />
-        <button on:click={generateIcal}>Opslaan in agenda</button>
-      </div>
+			<div class="row">
+				<CalendarPlus />
+				<button on:click={generateIcal}>Opslaan in agenda</button>
+			</div>
 
-      <hr />
+			<hr />
 
-      <div id="description">
-        <span>{activity.description}</span>
-      </div>
-    </div>
+			<div id="description">
+				<span>{activity.description}</span>
+			</div>
+		</div>
 
-    <div id="right" class="col">
-      <h2>Wie komen er allemaal?</h2>
+		<div id="right" class="col">
+			<h2>Wie komen er allemaal?</h2>
 
-      <hr />
+			<hr />
 
-      <div id="buttons">
-        <button on:click={async () => await setAttending(true)} id="bij-button">Ik ben 🐝!</button>
+			<div id="buttons">
+				<button on:click={async () => await setAttending(true)} id="bij-button">Ik ben 🐝!</button>
 
-        <hr />
+				<hr />
 
-        <button on:click={async () => await setAttending(false)} id="nietbij-button">Ik ben niet 🐝</button>
-      </div>
+				<button on:click={async () => await setAttending(false)} id="nietbij-button"
+					>Ik ben niet 🐝</button
+				>
+			</div>
 
-      <hr />
+			<hr />
 
-      <div id="users">
-        {#each bij as user}
-          <UserCard status="positive" user={user}/>
-        {/each}
-        {#each unsure as user}
-          <UserCard status="unsure" user={user}/>
-        {/each}
-        {#each notBij as user}
-          <UserCard status="negative" user={user}/>
-        {/each}
-      </div>
-
-    </div>
-  </div>
+			<div id="users">
+				{#each bij as user}
+					<UserCard status="positive" {user} />
+				{/each}
+				{#each unsure as user}
+					<UserCard status="unsure" {user} />
+				{/each}
+				{#each notBij as user}
+					<UserCard status="negative" {user} />
+				{/each}
+			</div>
+		</div>
+	</div>
 </div>
 
 <style lang="scss">
-  $border-radius: 10px;
-  $gap: 0.5rem;
-  $gap-side: 2rem;
+	$border-radius: 10px;
+	$gap: 0.5rem;
+	$gap-side: 2rem;
 
-  @media (max-width: 640px) {
-    #title {
-      display: none;
-    }
-  }
+	@media (max-width: 640px) {
+		#title {
+			display: none;
+		}
+	}
 
-  .cols {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-auto-rows: auto;
-    margin: 0 $gap-side;
+	.cols {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-auto-rows: auto;
+		margin: 0 $gap-side;
 
+		@media (max-width: 640px) {
+			grid-template-columns: 1fr;
+			margin: 0;
+			gap: $gap;
+		}
+	}
 
-    @media (max-width: 640px) {
-      grid-template-columns: 1fr;
-      margin: 0;
-      gap: $gap;
-    }
-  }
+	.col {
+		display: flex;
+		flex-direction: column;
 
-  .col {
-    display: flex;
-    flex-direction: column;
+		margin: $gap;
 
-    margin: $gap;
+		border: 1px solid var(--seperator-color);
+		border-radius: $border-radius;
 
-    border: 1px solid var(--seperator-color);
-    border-radius: $border-radius;
+		@media (max-width: 640px) {
+			margin: 0;
+			margin-bottom: $gap;
+			width: 90vw;
+		}
+	}
 
-    @media (max-width: 640px) {
-      margin: 0;
-      margin-bottom: $gap;
-      width: 90vw;
-    }
-  }
+	#left {
+		justify-content: flex-start;
 
-  #left {
-    justify-content: flex-start;
+		h2,
+		p,
+		.row,
+		button {
+			padding-left: 1rem;
+		}
 
-    h2, p, .row, button {
-      padding-left: 1rem;
-    }
+		a,
+		button {
+			color: var(--link-color);
+		}
 
-    a, button {
-      color: var(--link-color);
-    }
+		h2 {
+			padding-top: 0.5rem;
+			padding-bottom: 0.5rem;
+		}
 
-    h2 {
-      padding-top: 0.5rem;
-      padding-bottom: 0.5rem;
-    }
+		.row {
+			display: flex;
+			align-items: center;
+			padding-bottom: 0.25rem;
+			padding-top: 0.25rem;
+		}
 
-    .row {
-      display: flex;
-      align-items: center;
-      padding-bottom: 0.25rem;
-      padding-top: 0.25rem;
-    }
+		#description {
+			margin: 0.5rem 1rem;
+		}
 
-    #description {
-      margin: 0.5rem 1rem;
-    }
+		img {
+			width: 100%;
+			object-fit: cover;
+			max-height: 200px;
+			border-top-left-radius: $border-radius;
+			border-top-right-radius: $border-radius;
+		}
 
-    img {
-      width: 100%;
-      object-fit: cover;
-      max-height: 200px;
-      border-top-left-radius: $border-radius;
-      border-top-right-radius: $border-radius;
-    }
+		hr {
+			width: 100%;
+			margin: 0.2rem;
+		}
+	}
 
-    hr {
-      width: 100%;
-      margin: 0.2rem;
-    }
-  }
+	#right {
+		align-items: center;
 
-  #right {
-    align-items: center;
+		h2 {
+			padding-bottom: 0.5rem;
+		}
 
-    h2 {
-      padding-bottom: 0.5rem;
-    }
+		hr {
+			width: 100%;
+			padding: 0;
+			margin: 0;
+		}
 
-    hr {
-      width: 100%;
-      padding: 0;
-      margin: 0;
-    }
+		#users {
+			display: grid;
+			grid-template-columns: 1fr 1fr;
+			width: 90%;
+			margin: $gap auto;
 
-    #users {
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-      width: 90%;
-      margin: $gap auto;
+			@media (max-width: 640px) {
+				grid-template-columns: 1fr;
+				width: 90%;
+			}
+		}
 
-      @media (max-width: 640px) {
-        grid-template-columns: 1fr;
-        width: 90%;
-      }
-    }
+		#buttons {
+			display: grid;
+			grid-template-columns: 1fr 1px 1fr;
+			width: 100%;
 
-    #buttons {
-      display: grid;
-      grid-template-columns: 1fr 1px 1fr;
-      width: 100%;
+			hr {
+				z-index: 1;
+				width: 1px;
+				padding: 20px 0;
+				margin: 0;
+				height: 100%;
+				background-color: var(--seperator-color);
+			}
+		}
+	}
 
-      hr {
-        z-index: 1;
-        width: 1px;
-        padding: 20px 0;
-        margin: 0;
-        height: 100%;
-        background-color: var(--seperator-color);
-      }
-    }
-  }
+	h1 {
+		text-align: center;
+	}
 
-  h1 {
-    text-align: center;
-  }
-
-  hr {
-    margin: var(--hr-margin);
-  }
+	hr {
+		margin: var(--hr-margin);
+	}
 </style>
-
