9bf5791a7f0e8ecd5177d12920ecbd207f7a3f13
Niels <git@nierot.com>
2023-07-19 16:34:45 +0200

    chore: Add warning

--- src/routes/(app)/instellingen/+page.svelte
+++ src/routes/(app)/instellingen/+page.svelte
@@ -1,13 +1,26 @@
+<script lang="ts">
+	import { toast } from '$lib/notification';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		toast({
+			title: 'Instellingen',
+			message: 'Soon™',
+			type: 'warning'
+		});
+	});
+</script>
+
 <h1>Instellingen</h1>
 
 <hr />
 
 <style>
-  h1 {
-    text-align: center;
-  }
+	h1 {
+		text-align: center;
+	}
 
-  hr {
-    margin: 0.5rem 0;
-  }
+	hr {
+		margin: 0.5rem 0;
+	}
 </style>
