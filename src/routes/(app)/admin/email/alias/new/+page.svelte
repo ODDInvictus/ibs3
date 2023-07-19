e57363791f8760eb528f96d08687146e5af953df
Niels <git@nierot.com>
2023-07-19 16:18:42 +0200

    bug: fix bug

---
+++ src/routes/(app)/admin/email/alias/new/+page.svelte
@@ -0,0 +1,21 @@
+<h1>Email alias aanmaken</h1>
+
+<hr />
+
+<a href="/admin/email/alias/new/personal">Persoonlijke alias</a>
+<br />
+<a href="/admin/email/alias/new/custom">Custom alias</a>
+
+<style lang="scss">
+	h1 {
+		text-align: center;
+	}
+
+	hr {
+		margin: 0.5rem 0;
+	}
+
+	a {
+		color: var(--primary-color);
+	}
+</style>
