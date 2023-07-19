92dd286fc0998340655ad55bb43d99a6903a487d
Niels <git@nierot.com>
2023-07-19 16:35:04 +0200

    chore: Formatter

--- src/routes/(app)/strafbakken/Table.svelte
+++ src/routes/(app)/strafbakken/Table.svelte
@@ -1,140 +1,141 @@
 <script lang="ts">
-  import { Plus, Minus } from "svelte-heros-v2";
-  import Modal from "./Modal.svelte";
-  import { openModal } from "svelte-modals";
-  import type { sbUser } from "./types";
+	import Plus from '~icons/tabler/plus';
+	import Minus from '~icons/tabler/minus';
+	import Modal from './Modal.svelte';
+	import { openModal } from 'svelte-modals';
+	import type { sbUser } from './types';
 
-  export let data: sbUser[];
-  export let longestName: string | null;
+	export let data: sbUser[];
+	export let longestName: string | null;
 
-  const trekBak = (id: number, index: number) => {
-    changeCount(index, -1);
-    fetch("/strafbakken", {
-      method: "DELETE",
-      body: JSON.stringify({
-        user: id,
-      }),
-    }).catch(() => {
-      changeCount(index, 1);
-    });
-  };
+	const trekBak = (id: number, index: number) => {
+		changeCount(index, -1);
+		fetch('/strafbakken', {
+			method: 'DELETE',
+			body: JSON.stringify({
+				user: id
+			})
+		}).catch(() => {
+			changeCount(index, 1);
+		});
+	};
 
-  const changeCount = (index: number, n: number) => {
-    data[index]._count.StrafbakReceived += n;
-  };
+	const changeCount = (index: number, n: number) => {
+		data[index]._count.StrafbakReceived += n;
+	};
 </script>
 
 <main>
-  <thead>
-    <th>Naam</th>
-    <th>Bakken</th>
-    <th>Acties</th>
-  </thead>
-  <tbody>
-    {#each data as user, i}
-      <row class="row">
-        <a href={`/strafbakken/${user.firstName.toLowerCase()}`} class="cell">
-          {user.nickname || user.firstName}
-        </a>
-        <a href={`/strafbakken/${user.firstName.toLowerCase()}`} class="cell">
-          <p>{user._count.StrafbakReceived}</p>
-        </a>
-        <div class="actions cell">
-          <Plus
-            class="cursor-pointer hover:invert-[.35] z-0 focus:outline-0 duration-[400ms]"
-            on:click={() =>
-              openModal(Modal, {
-                username: user.nickname || user.firstName,
-                uid: user.id,
-                changeCount,
-                index: i,
-              })}
-          />
-          <Minus
-            class={user._count.StrafbakReceived
-              ? "cursor-pointer hover:invert-[.35] z-0 focus:outline-0 -translate-x-1 duration-[400ms]"
-              : "hidden"}
-            on:click={user._count.StrafbakReceived
-              ? () => trekBak(user.id, i)
-              : null}
-          />
-        </div>
-      </row>
-    {/each}
-    <!-- Add an invisible row with the longest name to make sure the 2 tables have the same width (kinda) -->
-    {#if longestName !== null}
-      <row id="invisible">
-        <p class="cell">{longestName}</p>
-        <div class="cell" />
-        <div class="cell" />
-      </row>
-    {/if}
-  </tbody>
+	<thead>
+		<th>Naam</th>
+		<th>Bakken</th>
+		<th>Acties</th>
+	</thead>
+	<tbody>
+		{#each data as user, i}
+			<row class="row">
+				<a href={`/strafbakken/${user.firstName.toLowerCase()}`} class="cell">
+					{user.nickname || user.firstName}
+				</a>
+				<a href={`/strafbakken/${user.firstName.toLowerCase()}`} class="cell">
+					<p>{user._count.StrafbakReceived}</p>
+				</a>
+				<div class="actions cell">
+					<button
+						on:click={() =>
+							openModal(Modal, {
+								username: user.nickname || user.firstName,
+								uid: user.id,
+								changeCount,
+								index: i
+							})}
+					>
+						<Plus class="cursor-pointer hover:invert-[.35] z-0 focus:outline-0 duration-[400ms]" />
+					</button>
+					<button on:click={user._count.StrafbakReceived ? () => trekBak(user.id, i) : null}>
+						<Minus
+							class={user._count.StrafbakReceived
+								? 'cursor-pointer hover:invert-[.35] z-0 focus:outline-0 -translate-x-1 duration-[400ms]'
+								: 'hidden'}
+						/>
+					</button>
+				</div>
+			</row>
+		{/each}
+		<!-- Add an invisible row with the longest name to make sure the 2 tables have the same width (kinda) -->
+		{#if longestName !== null}
+			<row id="invisible">
+				<p class="cell">{longestName}</p>
+				<div class="cell" />
+				<div class="cell" />
+			</row>
+		{/if}
+	</tbody>
 </main>
 
 <style lang="scss">
-  $cell-padding: 0.75rem;
+	$cell-padding: 0.75rem;
 
-  thead {
-    display: flex;
-    justify-content: space-around;
+	thead {
+		display: flex;
+		justify-content: space-around;
 
-    th {
-      padding: $cell-padding;
-      text-align: left;
-    }
-  }
+		th {
+			padding: $cell-padding;
+			text-align: left;
+		}
+	}
 
-  tbody {
-    display: grid;
-    grid-template-columns: 1fr;
+	tbody {
+		display: grid;
+		grid-template-columns: 1fr;
 
-    row {
-      display: grid;
-      grid-template-columns: repeat(3, minmax(0, 1fr));
-      transition: all 0.4s ease;
+		row {
+			display: grid;
+			grid-template-columns: repeat(3, minmax(0, 1fr));
+			transition: all 0.4s ease;
 
-      &:nth-child(odd) {
-        background-color: var(--primary-focus-color);
-        color: white;
-      }
+			&:nth-child(odd) {
+				background-color: var(--primary-focus-color);
+				color: white;
+			}
 
-      &:has(.cell:not(.actions):hover) {
-        background-color: var(--primary-color);
-        color: white;
-        text-decoration: underline;
-      }
+			&:has(.cell:not(.actions):hover) {
+				background-color: var(--primary-color);
+				color: white;
+				text-decoration: underline;
+			}
 
-      .cell {
-        padding: $cell-padding;
-        word-wrap: break-word;
-        position: relative;
+			.cell {
+				padding: $cell-padding;
+				word-wrap: break-word;
+				position: relative;
 
-        p {
-          position: absolute;
-          top: 50%;
-          transform: translateY(-50%);
-        }
-      }
+				p {
+					position: absolute;
+					top: 50%;
+					transform: translateY(-50%);
+				}
+			}
 
-      &#invisible {
-        opacity: 0;
-        cursor: default;
-        height: 0px;
+			&#invisible {
+				opacity: 0;
+				cursor: default;
+				height: 0px;
 
-        .cell {
-          padding: 0 $cell-padding;
-          word-wrap: normal;
-          text-overflow: clip;
-          line-height: 0%;
-        }
-      }
-    }
+				.cell {
+					padding: 0 $cell-padding;
+					word-wrap: normal;
+					text-overflow: clip;
+					line-height: 0%;
+				}
+			}
+		}
 
-    .actions {
-      display: flex;
-      align-items: center;
-      gap: $cell-padding;
-    }
-  }
+		.actions {
+			display: flex;
+			align-items: center;
+			gap: $cell-padding;
+		}
+	}
 </style>
