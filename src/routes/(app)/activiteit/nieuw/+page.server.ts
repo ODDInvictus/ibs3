a661e54b8f6b2abd0198e164a8c6c3b50c9469b8
Niels <git@nierot.com>
2023-07-23 14:25:35 +0200

    feat: Edit an activity

--- src/routes/(app)/activiteit/nieuw/+page.server.ts
+++ src/routes/(app)/activiteit/nieuw/+page.server.ts
@@ -1,17 +1,59 @@
 import db from '$lib/server/db'
-import { fail, redirect } from '@sveltejs/kit'
+import { error, fail, redirect } from '@sveltejs/kit'
 import fs from 'fs'
 import { env } from '$env/dynamic/private'
 import { LDAP_IDS } from '$lib/constants.js'
 import type { CommitteeMember } from '@prisma/client'
 import { z } from 'zod'
+import { pad } from '$lib/utils.js'
+import { authUser } from '$lib/server/authorizationMiddleware'
 
-export const load = (async ({ locals }) => {
+export const load = (async ({ url, locals }) => {
   const locations = db.activityLocation.findMany({
     where: {
       isActive: true
     }
   })
+
+  const edit = url.searchParams.get('edit')
+
+  if (edit === 'true') {
+    // Load up all the values
+    const id = url.searchParams.get('id')
+
+    if (id) {
+      const activity = await db.activity.findUnique({
+        where: {
+          id: parseInt(id)
+        },
+        include: {
+          location: true,
+          organisedBy: true
+        }
+      })
+
+      if (activity) {
+        // Split the date and time
+        const startDate = activity.startTime.toISOString().split('T')[0]
+        const startTime = pad(activity.startTime.getHours()) + ':' + pad(activity.startTime.getMinutes())
+        const endDate = activity.endTime.toISOString().split('T')[0]
+        const endTime = pad(activity.endTime.getHours()) + ':' + pad(activity.endTime.getMinutes())
+
+        return {
+          edit: true,
+          activity,
+          times: {
+            startDate,
+            startTime,
+            endDate,
+            endTime,
+          },
+          committees: locals.committees,
+          locations
+        }
+      }
+    }
+  }
 
   return {
     locations,
@@ -30,12 +72,12 @@ const validateTime = (time: string) => {
 }
 
 const formSchema = z.object({
-  name: z.string({ required_error: 'Naam is verplicht', invalid_type_error: 'Naam moet een string zijn' }).min(3, { message: 'Naam moet minimaal 3 karakters lang zijn'}),
+  name: z.string({ required_error: 'Naam is verplicht', invalid_type_error: 'Naam moet een string zijn' }).min(3, { message: 'Naam moet minimaal 3 karakters lang zijn' }),
   description: z.string({ required_error: 'Beschrijving is verplicht' }).min(3, { message: 'Beschrijving moet minimaal 3 karakters lang zijn' }),
-  startDate: z.string({ required_error: 'Begin datum is verplicht', invalid_type_error: 'Begin datum moet een datum zijn'}).refine(validateDate),
-  startTime: z.string({ required_error: 'Begin tijd is verplicht', invalid_type_error: 'Begin tijd moet een tijdstip zijn'}).refine(validateTime, { message: 'Begin tijd is niet valide' }),
-  endDate: z.string({ required_error: 'Eind datum is verplicht', invalid_type_error: 'Eind datum moet een datum zijn'}).refine(validateDate),
-  endTime: z.string({ required_error: 'Eind tijd is verplicht', invalid_type_error: 'Eind tijd moet een tijdstip zijn'}).refine(validateTime, { message: 'Eind tijd is niet valide' }),
+  startDate: z.string({ required_error: 'Begin datum is verplicht', invalid_type_error: 'Begin datum moet een datum zijn' }).refine(validateDate),
+  startTime: z.string({ required_error: 'Begin tijd is verplicht', invalid_type_error: 'Begin tijd moet een tijdstip zijn' }).refine(validateTime, { message: 'Begin tijd is niet valide' }),
+  endDate: z.string({ required_error: 'Eind datum is verplicht', invalid_type_error: 'Eind datum moet een datum zijn' }).refine(validateDate),
+  endTime: z.string({ required_error: 'Eind tijd is verplicht', invalid_type_error: 'Eind tijd moet een tijdstip zijn' }).refine(validateTime, { message: 'Eind tijd is niet valide' }),
   location: z.string({ invalid_type_error: 'LocatieID is onjuist' }).refine(val => {
     const num = parseInt(val)
     return !isNaN(num)
@@ -50,8 +92,16 @@ const formSchema = z.object({
 })
 
 export const actions = {
-  default: async ({ request }) => {
-    const data = Object.fromEntries(await request.formData())
+  default: async (event) => {
+    // do authorisation
+    const [authorized, committees] = authUser(event.locals)
+    if (!authorized) throw error(403, 'Helaas heb jij geen toegang tot deze actie. Je mist een van de volgende rollen: ' + committees.join(', '))
+
+    const data = Object.fromEntries(await event.request.formData())
+
+    console.log(event.url)
+    const edit = event.url.searchParams.get('edit') === 'true'
+    const editId = event.url.searchParams.get('id')
 
     const zodData = formSchema.safeParse(data)
 
@@ -66,7 +116,7 @@ export const actions = {
 
       return fail(400, { error: true, errors });
     }
-    
+
     const { name, description, startDate, startTime, endDate, endTime, location, organisedBy, url, image, membersOnly } = zodData.data
 
     const start = new Date(`${startDate} ${startTime}`)
@@ -88,20 +138,42 @@ export const actions = {
           loc = parseInt(location)
         }
 
-        const activity = await tx.activity.create({
-          data: {
-            name,
-            description,
-            startTime: start,
-            endTime: end,
-            locationId: loc,
-            membersOnly: membersOnly === 'on',
-            committeeId: parseInt(organisedBy),
-            url: url ?? null,
-          }
-        })
+        let activity
+
+        if (edit && editId) {
+          // Update the activity
+          activity = await tx.activity.update({
+            where: {
+              id: parseInt(editId)
+            },
+            data: {
+              name,
+              description,
+              startTime: start,
+              endTime: end,
+              locationId: loc,
+              membersOnly: membersOnly === 'on',
+              committeeId: parseInt(organisedBy),
+              url: url ?? null,
+            }
+          })
+        } else {
+          activity = await tx.activity.create({
+            data: {
+              name,
+              description,
+              startTime: start,
+              endTime: end,
+              locationId: loc,
+              membersOnly: membersOnly === 'on',
+              committeeId: parseInt(organisedBy),
+              url: url ?? null,
+            }
+          })
+        }
 
         id = activity.id
+
 
         if (image.size > 0) {
           const filename = `activiteit-${activity.id}-${image.name}`
@@ -124,44 +196,45 @@ export const actions = {
         // Create all attending objects
 
         // To do that: get all members + feuten
-        // and if membersOnly is set, then exclude the feuten
-        const members = await tx.committeeMember.findMany({
-          where: {
-            committee: {
-              ldapId: LDAP_IDS.MEMBERS
-            }
-          }
-        })
-
-        let feuten: CommitteeMember[] = []
-
-        if (!membersOnly) {
-          feuten = await tx.committeeMember.findMany({
+        // and if membersOnly is set, then exclude the feuten\
+        if (!edit) {
+          const members = await tx.committeeMember.findMany({
             where: {
               committee: {
-                ldapId: LDAP_IDS.FEUTEN
+                ldapId: LDAP_IDS.MEMBERS
               }
             }
           })
-        }
 
-        const users = members.concat(feuten)
+          let feuten: CommitteeMember[] = []
 
-        // Create the attending create objects
-        const attending = []
-        
-        for (const user of users) {
-          attending.push({
-            userId: user.userId,
-            activityId: activity.id
+          if (!membersOnly) {
+            feuten = await tx.committeeMember.findMany({
+              where: {
+                committee: {
+                  ldapId: LDAP_IDS.FEUTEN
+                }
+              }
+            })
+          }
+
+          const users = members.concat(feuten)
+
+          // Create the attending create objects
+          const attending = []
+
+          for (const user of users) {
+            attending.push({
+              userId: user.userId,
+              activityId: activity.id
+            })
+          }
+
+          // Create all attending objects!
+          await tx.attending.createMany({
+            data: attending
           })
         }
-
-        // Create all attending objects!
-        await tx.attending.createMany({
-          data: attending
-        })
-
       })
     } catch (e) {
       console.error(e)
