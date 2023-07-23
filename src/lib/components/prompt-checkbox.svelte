99d0fef66b78eb518cf3c96698efe1b8283dcd13
Niels <git@nierot.com>
2023-07-23 17:13:42 +0200

    feat: add `PromptCheckbox`

---
+++ src/lib/components/prompt-checkbox.svelte
@@ -0,0 +1,113 @@
+<script lang="ts">
+	import { promptCheckboxStore } from '$lib/promptCheckbox';
+
+	let currentValue = false;
+
+	$: {
+		const prompt = $promptCheckboxStore;
+		if (prompt && prompt.title !== '' && prompt.message !== '') {
+			const dialog = document.querySelector('#prompt-checkbox-dialog') as HTMLDialogElement;
+			dialog?.showModal();
+		}
+	}
+
+	async function action(confirm: boolean) {
+		const dialog = document.querySelector('#prompt-checkbox-dialog') as HTMLDialogElement;
+		if (confirm) {
+			await $promptCheckboxStore.cb(currentValue);
+			currentValue = false;
+		}
+		dialog?.close();
+	}
+</script>
+
+<dialog id="prompt-checkbox-dialog">
+	<h1>{$promptCheckboxStore.title}</h1>
+
+	<p>{$promptCheckboxStore.message}</p>
+
+	<div id="checkbox">
+		<input
+			type="checkbox"
+			checked={$promptCheckboxStore.value}
+			on:change={(e) => {
+				// @ts-expect-error
+				return (currentValue = e.target?.value === 'on');
+			}}
+		/>
+	</div>
+
+	<div class="buttons">
+		<button class="ok" on:click={() => action(true)}>Opslaan</button>
+		<button on:click={() => action(false)}>Annuleren</button>
+	</div>
+</dialog>
+
+<style lang="scss">
+	$border: 10px;
+	$padding: 1rem;
+
+	dialog {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		min-width: 25rem;
+
+		background: var(--primary-color);
+		color: white;
+		border-radius: $border;
+		padding: 2rem;
+
+		z-index: 1000;
+	}
+
+	#checkbox {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+	}
+
+	input {
+		padding: 0.5rem;
+		border-radius: 2px;
+		border: none;
+	}
+
+	input[type='checkbox']:checked {
+		background-color: var(--secondary-color);
+	}
+
+	.ok {
+		background: var(--secondary-color);
+		border-radius: $border;
+		padding: 0.5rem;
+		width: 8rem;
+	}
+
+	h1,
+	p {
+		padding-bottom: $padding;
+	}
+
+	.buttons {
+		padding-top: 1rem;
+		width: 100%;
+		display: flex;
+		justify-content: flex-end;
+		gap: $padding;
+	}
+
+	// media query for phones
+	@media (max-width: 600px) {
+		#prompt-checkbox-dialog {
+			width: 550px !important;
+			min-width: auto;
+		}
+
+		.ok {
+			width: 6rem;
+		}
+	}
+</style>
---
+++ src/lib/promptCheckbox.ts
@@ -0,0 +1,19 @@
+import { writable } from 'svelte/store'
+
+type PromptCheckbox = {
+  title: string
+  message: string
+  value: boolean
+  cb: (value: boolean) => Promise<void>
+}
+
+export const promptCheckboxStore = writable<PromptCheckbox>({
+  title: '',
+  message: '',
+  value: false,
+  cb: async () => alert(),
+})
+
+export function promptCheckbox(c: PromptCheckbox) {
+  promptCheckboxStore.set(c)
+}
