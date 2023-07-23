8484eb02a72d5a55498e0de07a059974305e21a8
Niels <git@nierot.com>
2023-07-23 13:17:20 +0200

    feat: Add title component

---
+++ src/lib/components/title.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let title: string = '';
+	export let shortTitle: string | undefined = '';
+</script>
+
+<svelte:head>
+	{#if title}
+		<title>IBS :: {shortTitle || title}</title>
+	{:else}
+		<title>Invictus Bier Systeem</title>
+	{/if}
+</svelte:head>
+
+<h1>Nieuwe activiteit aanmaken</h1>
+
+<hr />
+
+<style>
+	h1 {
+		text-align: center;
+	}
+
+	hr {
+		margin: 0.5rem 0;
+	}
+</style>
