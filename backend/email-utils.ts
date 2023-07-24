5d467ac3f39c045237d8df4fa55e96533de299b0
Niels <git@nierot.com>
2023-07-24 16:53:25 +0200

    feat: Send templated emails from the backend

--- backend/email-utils.ts
+++ backend/email-utils.ts
@@ -1,42 +1,109 @@
+import { User } from '@prisma/client'
+import Email from 'email-templates'
 import { createTransport } from 'nodemailer'
 
 const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD } = process.env
+
+const { NODE_ENV } = process.env
 
 if (!SMTP_HOST || !SMTP_PORT || !SMTP_USER || !SMTP_PASSWORD) {
   console.error('Missing required environment variables for emailer')
 }
 
-const transporter = createTransport({
-  host: SMTP_HOST,
-  port: parseInt(SMTP_PORT || '465'),
-  secure: false,
-  tls: {
-    ciphers: 'SSLv3'
-  },
-  auth: {
-    user: SMTP_USER,
-    pass: SMTP_PASSWORD
-  }
-})
+const dev = NODE_ENV === 'development'
 
-export async function sendEmailNotification(title: string, text: string, receiver: string) {
-  const info = await transporter.sendMail({
-    from: process.env.EMAIL_BACKEND_SENDER,
-    to: receiver,
-    subject: title,
-    text: text
-  })
-
-  console.log(`Email sent to ${receiver}: ${info.messageId}`)
+function log(...args: any[]) {
+  console.log('[EMAILER]', ...args)
 }
 
-export async function sendEmailNotificationFrontend(title: string, text: string, receiver: string) {
-  const info = await transporter.sendMail({
+let emailTransport
+
+if (dev) {
+  emailTransport = createTransport({
+    port: 1025,
+  })
+} else {
+  emailTransport = createTransport({
+    host: SMTP_HOST,
+    port: parseInt(SMTP_PORT || '465'),
+    secure: false,
+    tls: {
+      ciphers: 'SSLv3'
+    },
+    auth: {
+      user: SMTP_USER,
+      pass: SMTP_PASSWORD
+    },
+  })
+}
+
+const emailTemplateFrontend = new Email({
+  message: {
     from: process.env.EMAIL_SENDER,
-    to: receiver,
-    subject: title,
-    text: text
+    replyTo: process.env.EMAIL_REPLY_TO,
+  },
+  send: true,
+  preview: dev,
+  transport: emailTransport,
+})
+
+const emailTemplateBackend = new Email({
+  message: {
+    from: process.env.EMAIL_BACKEND_SENDER,
+    replyTo: process.env.ADMIN_EMAIL,
+  },
+  send: true,
+  preview: dev,
+  transport: emailTransport,
+})
+
+
+export async function sendEmailNotification(template: string, locals: Record<string, unknown>) {
+  const logo = process.env.EMAIL_IBS_LOGO
+
+  if (!logo) throw new Error('Missing EMAIL_IBS_LOGO in env')
+
+  const l = Object.assign(locals, {
+    url: process.env.IBS_URL,
+    name: 'Admin',
+    isAdmin: true,
+    logo
   })
 
-  console.log(`Email sent to ${receiver}: ${info.messageId}`)
+  const adminEmail = process.env.ADMIN_EMAIL
+
+  if (!adminEmail) throw new Error('Missing ADMIN_EMAIL in env')
+
+  await emailTemplateBackend.send({
+    template: template,
+    message: {
+      to: adminEmail,
+    },
+    locals: l
+  }).then(() => {
+    log('Email sent to admin')
+  })
+}
+
+export async function sendEmailNotificationFrontend(template: string, receiver: User, locals: Record<string, unknown>) {
+  const logo = process.env.EMAIL_IBS_LOGO
+
+  if (!logo) throw new Error('Missing EMAIL_IBS_LOGO in env')
+
+  const l = Object.assign(locals, {
+    url: process.env.IBS_URL,
+    name: receiver.firstName,
+    isMember: receiver.becameMember !== null,
+    logo
+  })
+
+  await emailTemplateFrontend.send({
+    template: template,
+    message: {
+      to: `${receiver.firstName} ${receiver.lastName} <${receiver.email}>`,
+    },
+    locals: l
+  }).then(() => {
+    log('Email sent to user:', receiver.ldapId)
+  })
 }
