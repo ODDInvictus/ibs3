fd59ae3b29bca33f854e7dc7d2cb699bc174871f
Niels <git@nierot.com>
2023-07-26 14:02:01 +0200

    cleanup: Remove settings from global object

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,12 @@
 /// <reference types="@auth/sveltekit" />
 /// <reference types="unplugin-icons/types/svelte" />
-import type { Committee, User, Settings } from 'prisma/prisma-client'
+import type { Committee, User } from 'prisma/prisma-client'
 
 declare global {
   declare namespace App {
     interface Locals {
       user: User
       committees: Committee[]
-      settings: Settings[]
     }
   }
 }
