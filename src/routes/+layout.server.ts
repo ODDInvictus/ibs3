11e5a197037e4a53db84729cbfae2e49ee762d65
Niels <git@nierot.com>
2023-07-26 14:02:14 +0200

    cleanup: remove currentRoute from global object

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,7 +2,6 @@ import type { LayoutServerLoad } from './$types'
 
 export const load: LayoutServerLoad = async (event) => {
 	const session = await event.locals.getSession();
-	const currentRoute = event.route
 	const user = event.locals.user
 	const committees = event.locals.committees
 
@@ -10,6 +9,5 @@ export const load: LayoutServerLoad = async (event) => {
 		session,
 		user,
 		committees,
-		currentRoute,
 	};
 };
