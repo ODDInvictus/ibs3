94f3754f18cbd492934f6cdda6cd4e98c48dd5ad
Niels <git@nierot.com>
2023-07-28 16:13:24 +0200

    style: New auth screen

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,59 +1,220 @@
 <script lang="ts">
 	import { signIn } from '@auth/sveltekit/client';
-	import Logo from '$lib/components/Logo.svelte';
-	import StyledButton from '$lib/components/StyledButton.svelte';
+	import Logo from '$lib/components/logo-v2-small.svelte';
+	import LogoBig from '$lib/components/logo-v2.svelte';
+	import Login from '~icons/tabler/login.svg';
+	import OAuth from '~icons/tabler/brand-oauth.svg';
+	import Register from '~icons/tabler/bookmark-edit.svg';
+	import { env } from '$env/dynamic/public';
+
+	let selection = 'login';
 </script>
 
-<div id="content" class="dark flex justify-center items-center">
-	<div class="-mt-20">
-		<div id="logo" />
-		<h1>
-			Invictus Bier Systeem
-		</h1>
-	
-		<div class="flex justify-center">
-			<button on:click={() => signIn('authentik')}> Log in </button>
+<main>
+	<div class="container">
+		<div class="welcome-card">
+			<header class="ibs-card">
+				<Logo />
+				<LogoBig />
+				<div class="spacer" />
+				<div class="buttons">
+					<button on:click={() => (selection = 'login')}>
+						<i><Login /></i>
+						Login
+					</button>
+					<button on:click={() => signIn('authentik')}>
+						<i>
+							<OAuth />
+						</i>
+						Login met Authentik
+					</button>
+					<button on:click={() => (selection = 'register')}>
+						<i>
+							<Register />
+						</i>
+						Registreer
+					</button>
+				</div>
+			</header>
 		</div>
+		<div class="stripe" />
+		<div class="login-card">
+			<div class="ibs-card">
+				{#if selection === 'login'}
+					<h1>Login</h1>
+					<input name="username" type="text" placeholder="gebruikersnaam" />
+					<input name="password" type="password" placeholder="wachtwoord" />
+					<button>Login</button>
+				{:else if selection === 'register'}
+					<h1>Registreer</h1>
+					<input name="username" type="text" placeholder="gebruikersnaam" />
+					<input name="code" type="text" placeholder="code" />
+					<button>Registreer</button>
+				{/if}
+			</div>
+		</div>
+		<footer>
+			<p>O.D.D. Invictus</p>
+			<p>
+				{env.PUBLIC_VERSION} -
+				<a href={env.PUBLIC_GITHUB_LINK + '/tree/' + env.PUBLIC_GIT_REV}>
+					({env.PUBLIC_GIT_REV_SHORT})
+				</a>
+			</p>
+		</footer>
 	</div>
-</div>
+</main>
 
 <style lang="scss">
-	#logo {
-		width: 600px;
-		height: 500px;
+	$padding: 2rem;
 
-		// Media query for mobile
-		@media (max-width: 600px) {
-			width: 300px;
-			height: 250px;
+	:global(body) {
+		background-color: var(--color-bg);
+	}
+
+	main {
+		height: 100%;
+		width: 100%;
+	}
+
+	.container {
+		position: relative;
+		z-index: 1;
+
+		height: 100%;
+		padding: 0 15%;
+
+		display: grid;
+		grid-template-columns: auto 1fr auto;
+		grid-template-rows: 1fr auto 1fr;
+		align-items: center;
+		column-gap: 4em;
+	}
+
+	.welcome-card {
+		width: max-content;
+
+		grid-row: 2;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		.ibs-card {
+			padding: $padding;
+
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
 		}
 
-		background-image: url('/logo_griffin.svg');
-		margin: 0;
+		.spacer {
+			height: 1.5rem;
+		}
+
+		.buttons {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+
+			gap: 1rem;
+
+			& > button {
+				width: 100%;
+			}
+		}
 	}
 
-	h1 {
-		font-size: 2rem;
+	.stripe {
+		position: absolute;
+		z-index: -1;
+		top: 0;
+		left: calc(15% - 1rem);
+		width: 11em;
+		height: 100%;
+
+		background-color: var(--color-primary);
+	}
+
+	.login-card {
+		grid-row: 2;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		.ibs-card {
+			padding: $padding;
+
+			display: grid;
+			gap: 1rem;
+		}
+	}
+
+	footer {
+		color: var(--color-text-muted);
+
+		align-self: end;
+		grid-column: span 2;
+		grid-row: 3;
+		padding-bottom: 2rem;
+
 		text-align: center;
-		color: white;
+
+		a {
+			text-decoration: none;
+		}
 	}
 
-	#content {
-		width: 100vw;
-		height: 100vh;
-		background-image: url('/auth_bg.svg');
-		background-size: cover;
-	}
+	@media (max-width: 600px) {
+		.container {
+			padding: 0 5%;
+			padding-top: 1rem;
 
-	button {
-		border-radius: 5px;
-		padding: 0.5rem 1rem;
-		color: white;
-	}
+			display: grid;
+			grid-template-columns: 1fr;
+			grid-template-rows: 1fr;
+			align-items: center;
+			row-gap: 1rem;
 
-	button:hover {
-		background-color: #f2f2f2;
-		color: #002233;
-		text-decoration: underline;
+			.welcome-card {
+				grid-row: 1;
+
+				width: 100%;
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+
+				.ibs-card {
+					width: calc(100% - 2 * #{$padding});
+				}
+
+				.ibs-card :global(svg) {
+					width: 60vw;
+				}
+			}
+
+			.stripe {
+				top: calc(15% - 1rem);
+				left: 0;
+				width: 100%;
+				height: 5rem;
+			}
+
+			.login-card {
+				grid-row: 2;
+				.ibs-card {
+					width: calc(100% - 2 * #{$padding});
+				}
+			}
+
+			footer {
+				grid-column: span 1;
+				grid-row: 3;
+			}
+		}
 	}
 </style>
