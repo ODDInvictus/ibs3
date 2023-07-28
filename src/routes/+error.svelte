13c0d8d62222e67a6945df75056abfd024023be7
Niels <git@nierot.com>
2023-07-28 16:12:39 +0200

    style: center error screen

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,28 +1,29 @@
 <script>
-  import { page } from '$app/stores';
+	import { page } from '$app/stores';
 </script>
 
 <div id="main">
-  <h1><code>{$page?.error?.message}</code></h1>
-  <p>
-    Daar is toch echt wel iets flink mis gegaan. 
-    Ik zou je een strafbak hiervoor geven maar helaas kan dat nog niet automatisch. 
-    Geluksvogel.
-  </p>
+	<h1><code>{$page?.error?.message}</code></h1>
+	<p>
+		Daar is toch echt wel iets flink mis gegaan. Ik zou je een strafbak hiervoor geven maar helaas
+		kan dat nog niet automatisch. Geluksvogel.
+	</p>
 </div>
 
-
 <style>
-  #main {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-  }
+	#main {
+		height: 100%;
+		width: 100%;
 
-  h1 {
-    font-size: 2.5rem;
-    margin: 0;
-  }
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+	}
+
+	h1 {
+		font-size: 2.5rem;
+		margin: 0;
+	}
 </style>
