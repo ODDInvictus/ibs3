797fa9ad9887d02981353c44a09f397828e7f900
Niels <git@nierot.com>
2023-07-30 19:01:58 +0200

    docs: Add component docs

---
+++ src/routes/(app)/docs/svelte/+page.svelte
@@ -0,0 +1,61 @@
+<script>
+	import Title from '$lib/components/title.svelte';
+	import Section from './_section.svelte';
+</script>
+
+<Title title="IBS UI toolkit" />
+
+<Section title="Buttons">
+	<button> Button </button>
+	<button class="btn-info"> Info button </button>
+	<button class="btn-warning"> Warning button </button>
+	<button class="btn-danger"> Danger button </button>
+	<button class="btn-success"> Success button </button>
+	<button class="btn-secondary"> Secondary button </button>
+</Section>
+
+<hr />
+
+<Section title="Typography">
+	<h1>Dit is een heading in h1</h1>
+	<h2 style="text-decoration: none;">Dit is een heading in h2</h2>
+	<h3>Dit is een heading in h3</h3>
+	<h4>Dit is een heading in h4</h4>
+	<h5>Dit is een heading in h5</h5>
+	<h6>Dit is een heading in h6</h6>
+	<p>Dit is een paragraph</p>
+</Section>
+
+<Section title="Form">
+	<input type="text" placeholder="text" />
+	<input type="number" placeholder="number" />
+	<input type="password" placeholder="password" />
+	<input type="email" placeholder="email" />
+
+	<textarea placeholder="textarea" />
+
+	<input type="checkbox" />
+	<input type="checkbox" checked />
+
+	<select>
+		<option>option 1</option>
+		<option>option 2</option>
+		<option>option 3</option>
+	</select>
+
+	<input type="radio" />
+
+	<input type="file" />
+
+	<input type="date" />
+
+	<input type="time" />
+
+	<input type="datetime-local" />
+
+	<input type="color" />
+
+	<input type="range" />
+</Section>
+
+<hr />
---
+++ src/routes/(app)/docs/svelte/_section.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const items = document.querySelectorAll(`.elements-${title} > *`) as NodeListOf<HTMLElement>;
+		// const code = document.querySelector(`.code-${title} code`) as HTMLElement;
+		const table = document.querySelector(`.items-${title}`) as HTMLElement;
+
+		items.forEach((item) => {
+			removeClasses(item);
+
+			// if the item has children, add newlines to all children
+			if (item.children.length > 0) {
+				item.innerHTML = item.innerHTML.replace(/</g, '\n<');
+
+				// also remove classes for children
+				item.querySelectorAll('*').forEach((child) => {
+					removeClasses(child);
+				});
+			}
+
+			const html = item.outerHTML;
+
+			const tr = document.createElement('tr');
+			const tdCode = document.createElement('td');
+			const tdElement = document.createElement('td');
+			const code = document.createElement('code');
+
+			code.innerText = html;
+			tdCode.appendChild(code);
+
+			tdElement.innerHTML = html;
+
+			tr.appendChild(tdCode);
+			tr.appendChild(tdElement);
+
+			table.appendChild(tr);
+		});
+	});
+
+	function removeClasses(element: Element) {
+		element.classList.forEach((c) => {
+			// Remove all classes that begin with s- or svelte- or data-svelte
+			console.log(c);
+			if (c.startsWith('s-') || c.includes('svelte')) {
+				element.classList.remove(c);
+			}
+		});
+
+		if (element.classList.length === 0) {
+			// Remove the class=""
+			element.removeAttribute('class');
+		}
+	}
+
+	export let title: string;
+</script>
+
+<section>
+	<h1>{title}</h1>
+
+	<div class="elements-{title}" style="display:none;">
+		<slot />
+	</div>
+
+	<table>
+		<thead>
+			<tr>
+				<th>Code</th>
+				<th>Element</th>
+			</tr>
+		</thead>
+		<tbody class="items-{title}" />
+	</table>
+</section>
+
+<style>
+	section {
+		margin: 1rem;
+	}
+</style>
