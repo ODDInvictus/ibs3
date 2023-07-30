5abd86201fccacb39e6665c819d7a9b8a8fd07f9
Niels <git@nierot.com>
2023-07-30 19:01:38 +0200

    docs: Add docs home page

---
+++ src/routes/(app)/docs/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Title from '$lib/components/title.svelte';
+</script>
+
+<Title title="IBS Documentatie" />
+
+<a href="/docs/markdown">Markdown</a>
+<a href="/docs/svelte">UI toolkit</a>
+<a href="/docs/api">API</a>
+
+<style>
+	a {
+		display: block;
+		margin: 1rem;
+	}
+</style>
