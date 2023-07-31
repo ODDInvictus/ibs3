30bf69387f1c52da4763a287bf7b0395d3050232
Niels <git@nierot.com>
2023-07-31 21:15:52 +0200

    cache: Add role cache

--- src/lib/server/userCache.ts
+++ src/lib/server/userCache.ts
@@ -2,6 +2,7 @@ import NodeCache from 'node-cache'
 import prisma from '$lib/server/db'
 import type { Session } from 'next-auth';
 import type { Committee, User } from '@prisma/client';
+import { UserRolesEmpty, Roles, type UserRoles } from '$lib/constants';
 
 // Keep user records for 24 hours
 const cache = new NodeCache({ stdTTL: 60 * 60 * 24 })
@@ -57,9 +58,42 @@ export const getCommittees = async (user: User | null): Promise<Committee[] | nu
 
 		cache.set(token, committees)
 		return committees
-		
+
 	} catch (e) {
 		console.error(e)
 		return null
 	}
+}
+
+export const getRoles = async (user: User | null, committees: Committee[] | null): Promise<UserRoles> => {
+	if (!user || !committees) {
+		return UserRolesEmpty
+	}
+
+	if (!committees) {
+		return UserRolesEmpty
+	}
+
+	const token = user.ldapId + '_roles'
+
+	if (cache.has(token)) {
+		return cache.get(token) as UserRoles
+	}
+
+	const roles = committees.map(committee => committee.ldapId)
+
+	let userRoles = UserRolesEmpty
+
+	const allRoles: string[] = Object.values(Roles)
+
+	for (const role of roles) {
+		if (allRoles.indexOf(role) !== -1) {
+			// @ts-expect-error
+			userRoles[role] = true
+		}
+	}
+
+	cache.set(token, userRoles)
+
+	return userRoles
 }
