1da7c830cd155ddf13b3cd2527cb5ee9c8ee491d
Niels <git@nierot.com>
2023-07-31 21:15:43 +0200

    toast: Add `danger` type

--- src/lib/notification.ts
+++ src/lib/notification.ts
@@ -3,13 +3,14 @@ import { writable } from 'svelte/store'
 type Notification = {
   title: string
   message: string
-  type: 'success' | 'error' | 'warning' | 'info'
+  type: 'success' | 'danger' | 'warning' | 'info'
   time?: number
 }
 
 export const notifications = writable<Notification[]>([])
 
 export function toast(notification: Notification) {
+  console.log('Toast called')
   notifications.update((notifications) => [...notifications, notification])
   setTimeout(removeToast, notification.time ?? 5000)
 }
