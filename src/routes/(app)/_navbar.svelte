762d394e5aa563913669044703cd79589ed37536
Niels <git@nierot.com>
2023-07-31 21:38:34 +0200

    style: Add gaps to navbar

--- src/routes/(app)/_navbar.svelte
+++ src/routes/(app)/_navbar.svelte
@@ -15,63 +15,49 @@
 </script>
 
 <nav class="layout--navbar">
-	<Logo />
+	<div class="logo">
+		<Logo />
+	</div>
 
 	<hr />
 
-	<div class="nav-item">
-		<a href="/kalender">
-			<i><Calendar /></i>
-			Kalender
-		</a>
-	</div>
-	<div class="nav-item">
-		<a href="/strafbakken">
-			<i><Beer /></i>
-			Strafbakken
-		</a>
-	</div>
-	<div class="nav-item">
-		<a href="/financieel">
-			<i><PigMoney /></i>
-			Financieel
-		</a>
-	</div>
+	<a class="layout--navbar--item" href="/kalender">
+		<i><Calendar /></i>
+		Kalender
+	</a>
+	<a class="layout--navbar--item" href="/strafbakken">
+		<i><Beer /></i>
+		Strafbakken
+	</a>
+	<a class="layout--navbar--item" href="/financieel">
+		<i><PigMoney /></i>
+		Financieel
+	</a>
 	{#if !$page.data.roles[LDAP_IDS.FEUTEN]}
-		<div class="nav-item">
-			<a href="/maluspunten">
-				<i><AccessibleOff /></i>
-				Maluspunten
-			</a>
-		</div>
+		<a class="layout--navbar--item" href="/maluspunten">
+			<i><AccessibleOff /></i>
+			Maluspunten
+		</a>
 	{/if}
-	<div class="nav-item">
-		<a href="/leden">
-			<i><Users /></i>
-			Leden
-		</a>
-	</div>
-	<div class="nav-item">
-		<a href="/instellingen">
-			<i><Settings /></i>
-			Instellingen
-		</a>
-	</div>
+	<a class="layout--navbar--item" href="/leden">
+		<i><Users /></i>
+		Leden
+	</a>
+	<a class="layout--navbar--item" href="/instellingen">
+		<i><Settings /></i>
+		Instellingen
+	</a>
 	{#if $page.data.roles[LDAP_IDS.ADMINS] || $page.data.roles[LDAP_IDS.SENAAT]}
-		<div class="nav-item">
-			<a href="/admin">
-				<i><ShieldCheck /></i>
-				Admin
-			</a>
-		</div>
+		<a class="layout--navbar--item" href="/admin">
+			<i><ShieldCheck /></i>
+			Admin
+		</a>
 	{/if}
 
-	<div class="nav-item version">
-		<a href="/over">
-			<i><InformationCircle /></i>
-			IBS v{env.PUBLIC_VERSION}
-		</a>
-	</div>
+	<a class="layout--navbar--item version" href="/over">
+		<i><InformationCircle /></i>
+		IBS v{env.PUBLIC_VERSION}
+	</a>
 </nav>
 
 <style lang="scss">
@@ -81,12 +67,19 @@
 		text-align: start;
 	}
 
-	.nav-item {
-		margin: 0.5rem 2rem;
+	.logo {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.logo :global(svg) {
+		width: 100%;
+		height: 100%;
+		margin: 0 1rem;
 	}
 
 	.version {
 		margin-top: auto;
-		margin-bottom: 1rem;
 	}
 </style>
