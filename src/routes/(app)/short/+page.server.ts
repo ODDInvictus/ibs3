cbd20e239b7e32c097e500836bbe6f8b5a197509
Niels <git@nierot.com>
2023-08-02 14:57:09 +0200

    style: Fix shortner

--- src/routes/(app)/short/+page.server.ts
+++ src/routes/(app)/short/+page.server.ts
@@ -1,45 +1,13 @@
-import db from '$lib/server/db';
-import { fail, redirect } from '@sveltejs/kit';
+import { shortLinkForm } from './form';
 
-export const actions = {
-  default: async ({ request, locals }) => {
-    // Get the request form
-    const form = await request.formData();
+export const load = (async ({ locals }) => {
 
-    const url = String(form.get('url'));
-    let slug = String(form.get('slug'));
+  await shortLinkForm.transform(locals.user)
 
-    if (!slug) {
-      // Generate an unique slug
-      slug = Math.random().toString(36).substring(2, 7);
-    }
-
-    // if the generated slug is already in use, just give up haha
-
-    if (slug) {
-      // Check if the slug is already taken
-      const existing = await db.link.findFirst({
-        where: {
-          shortLink: slug
-        }
-      })
-
-      if (existing) {
-        return fail(400, {
-          error: 'Verkorting is al in gebruik'
-        })
-      }
-    }
-
-    // Create the link
-    await db.link.create({
-      data: {
-        link: url,
-        shortLink: slug,
-        userId: locals.user.id,
-      }
-    })
-
-    throw redirect(302, '/short/success?slug=' + slug)
+  return {
+    form: shortLinkForm.attributes
   }
-}
+
+})
+
+export const actions = shortLinkForm.actions
--- src/routes/(app)/short/+page.svelte
+++ src/routes/(app)/short/+page.svelte
@@ -1,66 +1,8 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
 	import { page } from '$app/stores';
-	import Title from '$lib/components/title.svelte';
-	import type { ActionData } from './$types';
+	import Form from '$lib/form/form.svelte';
 
-	export let form: ActionData;
+	const f = $page.data.form;
 </script>
 
-<Title title="Maak een kortere link aan!" shortTitle="Link shortner" />
-
-{#if form?.error}
-	<p class="error">{form.error}</p>
-{/if}
-
-<form action="short" method="POST">
-	<div>
-		<label for="url">Link</label>
-		<input
-			type="url"
-			name="url"
-			id="url"
-			placeholder="https://www.youtube.com/watch?v=dgha9S39Y6M"
-			required
-		/>
-
-		<label for="slug">Verkorting</label>
-		<input type="text" name="slug" id="slug" placeholder="muziek" />
-	</div>
-
-	<p>Als je niks kiest wordt er iets gegenereerd</p>
-
-	<button type="submit">Verkorten</button>
-</form>
-
-<style lang="scss">
-	.error {
-		color: red;
-	}
-
-	form {
-		div {
-			margin-top: 1rem;
-			display: grid;
-			grid-template-columns: 150px 1fr;
-			gap: 1rem;
-
-			@media screen and (max-width: 600px) {
-				grid-template-columns: 1fr;
-
-				label + input {
-					margin-bottom: 0;
-				}
-			}
-		}
-
-		label {
-			font-weight: 600;
-			padding-top: 0.4rem;
-		}
-
-		button {
-			margin-top: 1rem;
-		}
-	}
-</style>
+<Form {...f} />
