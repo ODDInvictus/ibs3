7c8294edc4c0348ecfe16abbd49f26ecd73d36ef
Niels Rotmensen <git@nierot.com>
2023-08-08 23:54:07 +0200

    style: fix knoppers on mobile

--- src/routes/(app)/maluspunten/+error.svelte
+++ src/routes/(app)/maluspunten/+error.svelte
@@ -11,7 +11,7 @@
 
 <div><img src={knoppers} alt="Knopper" /></div>
 
-<style>
+<style lang="scss">
 	p {
 		text-align: center;
 	}
@@ -21,5 +21,12 @@
 		width: 100%;
 		flex-direction: column;
 		align-items: center;
+
+		@media (max-width: 640px) {
+			img {
+				box-sizing: border-box;
+				width: 100%;
+			}
+		}
 	}
 </style>
