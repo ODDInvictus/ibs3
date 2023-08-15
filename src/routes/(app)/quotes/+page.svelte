bc98f7fc58c482d28b71415bef5a0f1e5b0c0840
Niels <git@nierot.com>
2023-08-15 20:30:09 +0200

    misc

---
+++ src/routes/(app)/quotes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Title from '$lib/components/title.svelte';
+</script>
+
+<Title title="Quotes" />
+
+<em
+	>"Ja ik wil dit wel helemaal gaan bouwen enzo, maar dat lijkt me echt iets voor Naut om te maken"</em
+> - Niels
