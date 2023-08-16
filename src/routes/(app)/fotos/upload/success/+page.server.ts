aa9eae5e379cc63e0c7e8f456c24bc4f4162f94a
Niels <git@nierot.com>
2023-08-16 20:41:36 +0200

    WIP: Start working on tagging page

---
+++ src/routes/(app)/fotos/upload/success/+page.server.ts
@@ -0,0 +1,35 @@
+import type { PageServerLoad } from './$types';
+import db from '$lib/server/db'
+
+export const load = (async ({ url, locals }) => {
+  // Get ids from the query and get the photo objects
+  // from the database
+
+  const idsParam = url.searchParams.get('ids') as string
+
+  const ids = idsParam.split(',').map(id => parseInt(id))
+
+  const photos = db.photo.findMany({
+    where: {
+      id: {
+        in: ids
+      }
+    },
+    include: {
+      creator: true,
+      tags: {
+        include: {
+          photoTag: true
+        }
+      },
+      ratings: true,
+      peopleTagged: true,
+    },
+  })
+
+  const tags = db.photoTag.findMany()
+
+  const photoCreators = db.photoCreator.findMany()
+
+  return { photos, photoCreators, tags };
+}) satisfies PageServerLoad;
---
+++ src/routes/(app)/fotos/upload/success/+page.svelte
@@ -0,0 +1,295 @@
+<script lang="ts">
+	import Title from '$lib/components/title.svelte';
+	import type { PageData } from './$types';
+	import EditIcon from '~icons/tabler/edit.svelte';
+	import ArrowBackUp from '~icons/tabler/arrow-back-up.svelte';
+	import SaveIcon from '~icons/tabler/device-floppy.svelte';
+	import TagIcon from '~icons/mdi/tag-add.svelte';
+	import CirclePlus from '~icons/tabler/circle-plus.svelte';
+
+	export let data: PageData;
+
+	type Field = {
+		field: string;
+		value: string;
+	};
+
+	let editFields: Record<number, Field> = {};
+
+	console.log(data);
+
+	$: console.log(editFields);
+
+	function edit(field: string, photo: number) {
+		if (editFields[photo] && editFields[photo].field === field) {
+			// Discard edit
+			editFields[photo].field = '';
+			editFields[photo].value = '';
+		} else {
+			// Start edit
+			editFields[photo] = {
+				field: field,
+				value: ''
+			};
+		}
+	}
+
+	function save(field: string, photo: number) {
+		const value = editFields[photo].value;
+
+		if (!value) {
+			editFields[photo].field = '';
+			return;
+		}
+
+		console.log(`Field ${field} of photo ${photo} changed to ${value}!`);
+
+		// find the photo
+		for (const p of data.photos) {
+			if (p.id === photo) {
+				// @ts-ignore
+				switch (field) {
+					case 'date':
+						p.date = new Date(value);
+						break;
+					case 'name':
+						// @ts-ignore kan gewoon
+						p.PhotoCreator = data.photoCreators.find((c) => c.id === parseInt(value));
+						break;
+					case 'description':
+						p.description = value;
+						break;
+				}
+			}
+		}
+
+		editFields[photo].field = '';
+		editFields[photo].value = '';
+	}
+</script>
+
+<Title
+	title="Success! Uploaden gelukt"
+	shortTitle="Tag foto's"
+	underTitle="De backend is nog bezig met het verwerken van alle foto's, maar in de tussentijd kan je wel alvast metadata specificeren."
+/>
+
+<div class="image-container">
+	{#each data.photos as photo}
+		<div class="image">
+			<div class="options">
+				<table class="equal-width">
+					<thead>
+						<tr>
+							<th colspan="3">Metadata</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Gemaakt door</td>
+							{#if editFields[photo.id] && editFields[photo.id].field === 'name'}
+								<td>
+									<select
+										value={photo.creator.id}
+										on:change={(e) => {
+											if (e) {
+												// @ts-ignore
+												editFields[photo.id].value = e.target?.value;
+											}
+										}}
+									>
+										{#each data.photoCreators as creator}
+											<option value={creator.id}>{creator.name}</option>
+										{/each}
+									</select>
+								</td>
+								<td>
+									<button class="btn-a" on:click={() => edit('name', photo.id)}>
+										<i><ArrowBackUp /></i>
+									</button>
+									<button class="btn-a" on:click={() => save('name', photo.id)}>
+										<i><SaveIcon /></i>
+									</button>
+								</td>
+							{:else}
+								<td>
+									{photo.creator.name}
+								</td>
+								<td>
+									<button class="btn-a" on:click={() => edit('name', photo.id)}>
+										<i><EditIcon /></i>
+									</button>
+								</td>
+							{/if}
+						</tr>
+						<tr>
+							<td>Datum</td>
+							{#if editFields[photo.id] && editFields[photo.id].field === 'date'}
+								<td>
+									<input
+										type="date"
+										value={photo.date?.toISOString().substring(0, 10)}
+										on:change={(e) => {
+											if (e) {
+												// @ts-ignore
+												editFields[photo.id].value = e.target?.value;
+											}
+										}}
+									/>
+								</td>
+								<td>
+									<button class="btn-a" on:click={() => edit('date', photo.id)}>
+										<i><ArrowBackUp /></i>
+									</button>
+									<button class="btn-a" on:click={() => save('date', photo.id)}>
+										<i><SaveIcon /></i>
+									</button>
+								</td>
+							{:else}
+								<td>
+									<time datetime={photo.date?.toLocaleDateString('nl')}>
+										{photo.date?.toLocaleDateString('nl')}
+									</time>
+								</td>
+								<td>
+									<button class="btn-a" on:click={() => edit('date', photo.id)}>
+										<i><EditIcon /></i>
+									</button>
+								</td>
+							{/if}
+						</tr>
+						<tr>
+							<td>Beschrijving</td>
+							{#if editFields[photo.id] && editFields[photo.id].field === 'description'}
+								<td>
+									<textarea
+										value={photo.description}
+										on:change={(e) => {
+											if (e) {
+												// @ts-ignore
+												editFields[photo.id].value = e.target?.value;
+											}
+										}}
+									/>
+								</td>
+								<td>
+									<button class="btn-a" on:click={() => edit('description', photo.id)}>
+										<i><ArrowBackUp /></i>
+									</button>
+									<button class="btn-a" on:click={() => save('description', photo.id)}>
+										<i><SaveIcon /></i>
+									</button>
+								</td>
+							{:else}
+								<td>
+									{photo.description ?? 'Geen beschrijving'}
+								</td>
+								<td>
+									<button class="btn-a" on:click={() => edit('description', photo.id)}>
+										<i><EditIcon /></i>
+									</button>
+								</td>
+							{/if}
+						</tr>
+						<tr>
+							<td>Tags</td>
+							{#if editFields[photo.id] && editFields[photo.id].field === 'tags'}
+								{@const tags = data.tags.filter(
+									(t) => photo.tags.find((pt) => pt.photoTag.id === t.id) === undefined
+								)}
+								<td>
+									<select>
+										{#each tags as tag}
+											<option value={tag.id}>{tag.name}</option>
+										{/each}
+										<option value="-1">Nieuwe tag aanmaken</option>
+									</select>
+								</td>
+								<td>
+									<button class="btn-a" on:click={() => edit('tags', photo.id)}>
+										<i><ArrowBackUp /></i>
+									</button>
+									<button class="btn-a" on:click={() => save('tags', photo.id)}>
+										<i><CirclePlus /></i>
+									</button>
+								</td>
+							{:else}
+								<td>
+									{#each photo.tags as tag}
+										<span class="ibs-chip removable">{tag.photoTag.name}</span>
+									{/each}
+								</td>
+								<td>
+									<button class="btn-a" on:click={() => edit('tags', photo.id)}>
+										<i><TagIcon /></i>
+									</button>
+								</td>
+							{/if}
+						</tr>
+						<tr>
+							<td>Mensen</td>
+							<td>{photo.peopleTagged}</td>
+							<td><button class="btn-a"><i><EditIcon /></i></button></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="photo">
+				<img src="/upload/fotos/{photo.photo}" alt="Foto van {photo.creator.name}" />
+			</div>
+		</div>
+
+		<hr />
+	{/each}
+</div>
+
+<style lang="scss">
+	$img-height: 300px;
+
+	.image-container {
+		display: grid;
+
+		gap: 1rem;
+
+		grid-template-columns: 1fr;
+	}
+
+	.image {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+
+		height: $img-height;
+	}
+
+	.options {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		min-width: 100%;
+		table {
+			table-layout: fixed;
+
+			width: 100%;
+
+			textarea {
+				width: 100%;
+				text-align: start;
+			}
+		}
+	}
+
+	.photo {
+		display: flex;
+		justify-content: flex-start;
+
+		img {
+			height: $img-height;
+		}
+	}
+
+	.options {
+		width: 50%;
+	}
+</style>
