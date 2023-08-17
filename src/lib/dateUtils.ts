ef943670ba446e9d750bcc86ffc2ed1898e929f1
Niels <git@nierot.com>
2023-08-17 11:25:34 +0200

    utils: Add date utils

---
+++ src/lib/dateUtils.ts
@@ -0,0 +1,34 @@
+export function getDutchMonth(date: Date) {
+  switch (date.getMonth()) {
+    case 0:
+      return 'januari';
+    case 1:
+      return 'februari';
+    case 2:
+      return 'maart';
+    case 3:
+      return 'april';
+    case 4:
+      return 'mei';
+    case 5:
+      return 'juni';
+    case 6:
+      return 'juli';
+    case 7:
+      return 'augustus';
+    case 8:
+      return 'september';
+    case 9:
+      return 'oktober';
+    case 10:
+      return 'november';
+    case 11:
+      return 'december';
+    default:
+      return 'onbekend';
+  }
+}
+
+function toDateString(date: Date) {
+  return date.toLocaleDateString('nl')
+}
