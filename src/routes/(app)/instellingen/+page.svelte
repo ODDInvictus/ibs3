e498866a96077f2efb2ce0fda55472b74b96e4e1
Niels <56843328+Nierot@users.noreply.github.com>
2023-09-03 14:24:46 +0200

    feat: Set preferred theme

--- src/routes/(app)/instellingen/+page.svelte
+++ src/routes/(app)/instellingen/+page.svelte
@@ -6,6 +6,7 @@
 	import { promptCheckbox } from '$lib/promptCheckbox';
 	import { confirm } from '$lib/confirm';
 	import { toast } from '$lib/notification';
+	import type { PageData } from './$types';
 
 	const preferences = $page.data.preferences as Preference[];
 
@@ -67,9 +68,80 @@
 				console.error(err);
 			});
 	}
+
+	async function setTheme(theme: string) {
+		await fetch('', {
+			method: 'POST',
+			body: JSON.stringify({
+				action: 'theme',
+				theme
+			})
+		})
+			.then(async (res) => {
+				const body = await res.json();
+
+				if (body.success) {
+					toast({
+						title: 'Thema aangepast',
+						message: `De voorkeur is aangepast naar ${theme}.`,
+						type: 'success'
+					});
+					setTimeout(() => {
+						location.reload();
+					}, 1000);
+				} else {
+					toast({
+						title: 'Thema niet aangepast',
+						message: body.message,
+						type: 'danger'
+					});
+				}
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}
+
+	export let data: PageData;
 </script>
 
 <Title title="Instellingen" underTitle="Op deze pagina kan je je ibs voorkeuren veranderen." />
+
+<h2>Thema</h2>
+
+<input
+	type="radio"
+	id="light"
+	name="theme"
+	value="light"
+	checked={data.currentTheme === 'light'}
+	on:click={() => setTheme('light')}
+/>
+<label for="light">Lichte modus</label>
+<br />
+<input
+	type="radio"
+	id="dark"
+	name="theme"
+	value="dark"
+	on:click={() => setTheme('dark')}
+	checked={data.currentTheme === 'dark'}
+/>
+<label for="dark">Donkere modus</label>
+<br />
+<input
+	type="radio"
+	id="feut"
+	name="theme"
+	value="feut"
+	on:click={() => setTheme('feut')}
+	checked={data.currentTheme === 'feut'}
+/>
+<label for="feut">Feuten thema</label>
+
+<hr />
+
+<h2>Email voorkeuren</h2>
 
 <Table
 	class="striped"
