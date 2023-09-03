d68e3f31b101d7844d08c94854082556b5f1f3e9
Niels <56843328+Nierot@users.noreply.github.com>
2023-09-03 14:20:09 +0200

    feat: rename

--- src/routes/(app)/quotes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Title from '$lib/components/title.svelte';
-</script>
-
-<Title title="Quotes" />
-
-<em
-	>"Ja ik wil dit wel helemaal gaan bouwen enzo, maar dat lijkt me echt iets voor Naut om te doen"</em
-> - Niels
