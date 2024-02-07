86a00818e4a8bf14d4b776c487c022d212cd930d
Niels <56843328+Nierot@users.noreply.github.com>
2024-02-07 23:18:50 +0100

    feat: Generate iCal from activities and birthdays

--- src/lib/server/calendar/ical.ts
+++ src/lib/server/calendar/ical.ts
@@ -2,11 +2,17 @@ import { AttendingStatus } from '@prisma/client'
 import db from '../db'
 import ics from 'ics'
 import type { ParticipationStatus } from 'ics'
+import { activitySlug } from '$lib/textUtils'
+
+// https://icalendar.org/validator.html#results
 
 export async function generateActivityIcal() {
   // Get all upcoming activities
   const activities = await db.activity.findMany({
     where: {
+      startTime: {
+        gte: new Date()
+      }
     },
     include: {
       location: true,
@@ -35,53 +41,61 @@ export async function generateActivityIcal() {
     const hours = Math.abs(activity.endTime.getTime() - activity.startTime.getTime()) / 36e5
     const minutes = Math.abs(activity.endTime.getTime() - activity.startTime.getTime()) / 6e4
 
+    let description = activity.description ?? 'Geen beschrijving'
+    description += '\n\nLees meer op ' + process.env.ORIGIN + '/activiteit/' + activitySlug(activity.name) + '/' + activity.id
+
+
     const event: ics.EventAttributes = {
-      start: [activity.startTime.getFullYear(), activity.startTime.getMonth() + 1, activity.startTime.getDate()],
-      end: [activity.endTime.getFullYear(), activity.endTime.getMonth() + 1, activity.endTime.getDate()],
-      duration: { hours, minutes },
+      uid: `${activity.id}-${activitySlug(activity.name)}@${process.env.EMAIL_DOMAIN}`,
+      start: [activity.startTime.getFullYear(), activity.startTime.getMonth() + 1, activity.startTime.getDate(), activity.startTime.getHours(), activity.startTime.getMinutes()],
+      end: [activity.endTime.getFullYear(), activity.endTime.getMonth() + 1, activity.endTime.getDate(), activity.endTime.getHours(), activity.endTime.getMinutes()],
+      // duration: { hours, minutes },
       title: activity.name,
-      description: activity.description,
+      description,
       location: activity.location?.name ?? 'Locatie onbekend',
-      url: process.env.ORIGIN + '/activiteit/' + activity.id,
-      categories: ['O.D.D. Invictus', 'Activiteit'],
+      // Not supported by Google nor Proton
+      // url: process.env.ORIGIN + '/activiteit/' + activity.id,
+      categories: ['\"O.D.D. Invictus\"', 'Activiteit'],
       status: 'CONFIRMED',
-      busyStatus: 'BUSY',
+      // busyStatus: 'BUSY',
       organizer: {
         name: activity.organisedBy.name,
         email: `${activity.organisedBy.ldapId}@${process.env.EMAIL_DOMAIN}`
       },
-      attendees: activity.attending.map(member => {
-        let name;
-        if (member.user.nickname) {
-          name = `${member.user.firstName} "${member.user.nickname}" ${member.user.lastName}`
-        } else {
-          name = `${member.user.firstName} ${member.user.lastName}`
-        }
+      // TODO:
+      // Dit werkt niet, want we hebben geen google workspace
+      // attendees: activity.attending.map(member => {
+      //   let name;
+      //   if (member.user.nickname) {
+      //     name = `${member.user.firstName} \`${member.user.nickname}\` ${member.user.lastName}`
+      //   } else {
+      //     name = `${member.user.firstName} ${member.user.lastName}`
+      //   }
 
-        let partstat: ParticipationStatus
+      //   let partstat: ParticipationStatus
 
-        switch (member.status) {
-          case AttendingStatus.ATTENDING:
-            partstat = 'ACCEPTED'
-            break
-          case AttendingStatus.NOT_ATTENDING:
-            partstat = 'DECLINED'
-            break
-          case AttendingStatus.UNSURE:
-            partstat = 'TENTATIVE'
-            break
-          case AttendingStatus.NO_RESPONSE:
-            partstat = 'NEEDS-ACTION'
-            break
-        }
+      //   switch (member.status) {
+      //     case AttendingStatus.ATTENDING:
+      //       partstat = 'ACCEPTED'
+      //       break
+      //     case AttendingStatus.NOT_ATTENDING:
+      //       partstat = 'DECLINED'
+      //       break
+      //     case AttendingStatus.UNSURE:
+      //       partstat = 'TENTATIVE'
+      //       break
+      //     case AttendingStatus.NO_RESPONSE:
+      //       partstat = 'NEEDS-ACTION'
+      //       break
+      //   }
 
-        return {
-          name,
-          email: member.user.email,
-          role: 'OPT-PARTICIPANT',
-          partstat
-        }
-      })
+      //   return {
+      //     name,
+      //     email: member.user.email,
+      //     role: 'OPT-PARTICIPANT',
+      //     partstat
+      //   }
+      // })
     }
 
     events.push(event)
@@ -91,11 +105,12 @@ export async function generateActivityIcal() {
 
   members.forEach(member => {
     if (!member.birthDate) return
-    const date: [number, number, number] = [member.birthDate.getFullYear(), member.birthDate.getMonth(), member.birthDate.getDate()]
 
     const event: ics.EventAttributes = {
+      recurrenceRule: "FREQ=YEARLY",
+      uid: `${member.ldapId}-verjaardag@${process.env.EMAIL_DOMAIN}`,
       start: [member.birthDate.getFullYear(), member.birthDate.getMonth() + 1, member.birthDate.getDate()],
-      end: [member.birthDate.getFullYear(), member.birthDate.getMonth() + 1, member.birthDate.getDate() + 1],
+      duration: { days: 1 },
       title: `${member.firstName} jarig`,
       description: `${member.firstName} dit is je verjaardag, je verjaardag gaat zomaar niet voorbij!`,
       location: 'Bakken vouwen',
@@ -117,12 +132,14 @@ export async function generateActivityIcal() {
       ]
     }
 
-    // events.push(event)
+    events.push(event)
   })
 
   // Generate iCal
   const { error, value } = ics.createEvents(events, {
     calName: 'O.D.D. Invictus',
+    productId: '//ODDInvictus//ibs3//NL',
+    method: 'PUBLISH',
   })
 
   if (error) throw error
