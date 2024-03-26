99de7681885139694a25021d6a72d402eeaba4e7
Naut de Vroome <nautdevroome@gmail.com>
2024-03-26 13:55:01 +0100

    Fixed stream + added superforms

--- src/routes/(app)/file/+page.svelte
+++ src/routes/(app)/file/+page.svelte
@@ -1,15 +1,39 @@
 <script lang="ts">
+	import Title from '$lib/components/title.svelte';
+	import Submit from '$lib/superforms/Submit.svelte';
+	import { onError } from '$lib/superforms/error';
+	import { superForm } from 'sveltekit-superforms/client';
 	import type { PageData } from './$types';
+	import SuperFileField from '$lib/superforms/SuperFileField.svelte';
+	import { toast } from '$lib/notification';
 
 	export let data: PageData;
+
+	const formProps = superForm(data.form, {
+		onError,
+		onResult: ({ result }) => {
+			if (result.type === 'success') {
+				toast({
+					title: 'File upload succesvol',
+					message: `${result.data?.name} is opgeslagen`,
+					type: 'success'
+				});
+			} else {
+				toast({
+					message: 'File upload failed',
+					title: 'Error',
+					type: 'danger'
+				});
+			}
+		}
+	});
+
+	const { enhance } = formProps;
 </script>
 
-<h1>Mongo test pagina</h1>
+<Title title="File upload" />
 
-<form method="post" enctype="multipart/form-data">
-	<label for="file">
-		<span>Bestand</span>
-		<input type="file" name="file" required />
-	</label>
-	<button type="submit">Opslaan</button>
+<form method="post" enctype="multipart/form-data" class="superform" use:enhance>
+	<SuperFileField {formProps} name="file">File</SuperFileField>
+	<Submit {formProps}>Uploaden</Submit>
 </form>
--- src/routes/(app)/file/[filename]/+server.ts
+++ src/routes/(app)/file/[filename]/+server.ts
@@ -1,39 +1,26 @@
 import type { RequestHandler } from './$types';
 import Mongo from '$lib/server/mongo';
-import { GridFSBucket, type GridFSFile } from 'mongodb';
-
-async function getFile(name: string): Promise<{ buffer: Buffer; data: GridFSFile }> {
-	const gfs = new GridFSBucket(Mongo, { bucketName: 'fs' });
-	const cursor = gfs.find({ filename: name });
-	const doc = await cursor.next();
-	if (!doc) return Promise.reject({ code: 'ENOENT' });
-
-	return new Promise((resolve, reject) => {
-		const stream = gfs.openDownloadStreamByName(name);
-		const chunks: Buffer[] = [];
-		stream.start();
-		stream.on('data', (chunk) => chunks.push(chunk));
-		stream.on('end', async () => {
-			resolve({
-				buffer: Buffer.concat(chunks),
-				data: doc
-			});
-		});
-		stream.on('error', (err) => reject(err));
-		stream.end();
-	});
-}
+import { GridFSBucket } from 'mongodb';
 
 export const GET: RequestHandler = async ({ params }) => {
+	const filename = decodeURIComponent(params.filename);
+	const gfs = new GridFSBucket(Mongo, { bucketName: 'fs' });
+	const cursor = gfs.find({ filename });
+	const doc = await cursor.next();
+	if (!doc) return new Response(null, { status: 404 });
+
 	try {
-		const { buffer, data } = await getFile(params.filename);
-		return new Response(buffer, {
+		// TODO - fix this type issue
+		const stream = gfs.openDownloadStreamByName(filename) as unknown as ReadableStream;
+		return new Response(stream, {
 			headers: {
-				'Content-Type': data.metadata?.type ?? ''
+				'Content-Type': doc.metadata?.type ?? '',
+				'Cache-Control': 'public, max-age=31536000, immutable',
+				'Content-Length': doc.length.toString()
 			}
 		});
 	} catch (error: any) {
-		if (error?.code === 'ENOENT') {
+		if (error?.code === 'ENOENT' || error === 'ENOENT') {
 			return new Response(null, { status: 404 });
 		}
 		console.error(error);
