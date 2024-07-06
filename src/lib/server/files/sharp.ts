56b5120ad78b119ca46bc5e19dec8decba908cfd
Niels <56843328+Nierot@users.noreply.github.com>
2024-07-06 22:42:22 +0200

    #320 - Small changes

--- src/lib/server/files/sharp.ts
+++ src/lib/server/files/sharp.ts
@@ -1,7 +1,7 @@
 import { createRedisJob } from '../cache'
 
-export function rotateImage(filename: string, degrees: number) {
-	return createRedisJob('rotate-image', `${filename},${degrees}`)
+export async function rotateImage(filename: string, degrees: number) {
+	return await createRedisJob('rotate-image', `${filename},${degrees}`)
 }
 
 export async function compressImage(filename: string, quality = 90) {
