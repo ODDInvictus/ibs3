495483814061ef96fef0acada279d56c11648888
Niels <git@nierot.com>
2024-12-24 16:37:09 +0100

    feat: redirect to /home instead of /auth

---
+++ src/routes/(public)/home/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { PageData } from './$types'
+</script>
+
+<main>
+	<div class="center">
+		<img src="logo.png" alt="invictus logo" />
+	</div>
+</main>
+
+<style lang="scss">
+	:root {
+		height: 100vh;
+		width: 100vw;
+	}
+
+	main {
+		height: 100%;
+		width: 100%;
+		background-color: var(--color-primary);
+		color: var(--color-text-light);
+	}
+</style>
