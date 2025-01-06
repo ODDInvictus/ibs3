be7ab9d4c5136f636020b6cc225833379006be03
Niels <56843328+Nierot@users.noreply.github.com>
2025-01-06 15:26:24 +0100

    todo: public adtmeister page

---
+++ src/routes/(public)/adtmeister/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import type { PageData } from './$types'
+	import Menu from '~icons/tabler/menu-2'
+
+	export let data: PageData
+</script>
+
+<div class="root">
+	<header>O.D.D. Invictus Adtmeister klassement</header>
+
+	<main>Content hier</main>
+
+	<nav>
+		Navigatie hier
+		<div id="hamburger">
+			<button class="btn-a"><Menu /></button>
+		</div>
+	</nav>
+</div>
+
+<style lang="scss">
+	.root {
+		display: grid;
+		height: 100dvh;
+		grid-template-rows: 2rem 1fr 2rem;
+		grid-template-columns: 1fr;
+	}
+
+	nav {
+		display: grid;
+		height: 100px;
+		grid-template-columns: 1fr 1fr;
+
+		#hamburger {
+			display: block;
+			text-align: end;
+			padding: 10px;
+		}
+	}
+</style>
