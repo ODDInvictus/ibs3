ef1e871de03a48be78c7d041c7d1e88323d56871
Niels <56843328+Nierot@users.noreply.github.com>
2026-01-30 10:22:30 +0100

    feat: add /health check

--- src/routes/(public)/health/+server.ts
+++ src/routes/(public)/health/+server.ts
@@ -1,5 +1,11 @@
 import type { RequestHandler } from './$types'
 
 export const GET: RequestHandler = async () => {
+	log('Healthcheck performed')
 	return new Response('ok')
+}
+
+function log(...objects: any[]) {
+	const date = new Date(Date.now())
+	console.log(`[Health][${date.toLocaleDateString('nl')} ${date.toLocaleTimeString('nl')}]`, ...objects)
 }
