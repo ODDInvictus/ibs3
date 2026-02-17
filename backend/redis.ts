a479d2dce655ecb8c492b13830e6210a3a39fe8a
Niels <56843328+Nierot@users.noreply.github.com>
2026-02-17 12:57:47 +0100

    🦀🦀🦀 Weg backend

--- backend/redis.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from 'redis'
-
-const client = createClient({
-	url: process.env.REDIS_URL ?? 'redis://localhost:6379',
-})
-
-client.on('error', err => console.log('[REDIS] Client Error', err))
-
-export default client
